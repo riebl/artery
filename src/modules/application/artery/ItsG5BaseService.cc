8a1a6177299422fc98953a735f3446139e9dbe7e
Raphael Riebl <raphael.riebl@thi.de>
2014-07-16 13:26:58 +0200

    artery: ItsG5BaseService

---
+++ src/modules/application/artery/ItsG5BaseService.cc
@@ -0,0 +1,73 @@
+//
+// Copyright (C) 2014 Raphael Riebl <raphael.riebl@thi.de>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+
+#include "ItsG5Service.h"
+#include "ItsG5Middleware.h"
+#include "FindModule.h"
+#include <cassert>
+
+ItsG5BaseService::ItsG5BaseService() :
+		m_facilities(nullptr), m_middleware(nullptr)
+{
+}
+
+ItsG5BaseService::~ItsG5BaseService()
+{
+}
+
+Facilities& ItsG5BaseService::getFacilities()
+{
+	assert(m_facilities);
+	return *m_facilities;
+}
+
+ItsG5BaseService::port_type ItsG5BaseService::getPortNumber() const
+{
+	assert(m_middleware);
+	return m_middleware->getPortNumber(this);
+}
+
+cModule* ItsG5BaseService::findHost()
+{
+	return FindModule<>::findHost(this);
+}
+
+void ItsG5BaseService::initialize()
+{
+	ItsG5Middleware* middleware = dynamic_cast<ItsG5Middleware*>(getParentModule());
+	if (middleware == nullptr) {
+		opp_error("Middleware not found");
+	}
+
+	m_facilities = middleware->getFacilities();
+	m_middleware = middleware;
+}
+
+void ItsG5BaseService::trigger()
+{
+}
+
+void ItsG5BaseService::request(const vanetza::btp::DataRequestB& req, std::unique_ptr<vanetza::btp::DownPacket> packet)
+{
+	assert(m_middleware);
+	m_middleware->request(req, std::move(packet));
+}
+
+void ItsG5BaseService::indicate(const vanetza::btp::DataIndication& ind, std::unique_ptr<vanetza::btp::UpPacket> packet)
+{
+}
---
+++ src/modules/application/artery/ItsG5BaseService.h
@@ -0,0 +1,50 @@
+//
+// Copyright (C) 2014 Raphael Riebl <raphael.riebl@thi.de>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+
+#ifndef ITSG5BASESERVICE_H_
+#define ITSG5BASESERVICE_H_
+
+#include <csimplemodule.h>
+#include <vanetza/btp/data_interface.hpp>
+#include <vanetza/btp/data_request.hpp>
+#include "Facilities.h"
+#include "ItsG5Middleware.h"
+
+class ItsG5BaseService : public cSimpleModule, public vanetza::btp::IndicationInterface
+{
+	public:
+		typedef ItsG5Middleware::port_type port_type;
+
+		ItsG5BaseService();
+		virtual ~ItsG5BaseService();
+		virtual void trigger();
+
+	protected:
+		void initialize() override;
+		void request(const vanetza::btp::DataRequestB&, std::unique_ptr<vanetza::btp::DownPacket>);
+		void indicate(const vanetza::btp::DataIndication&, std::unique_ptr<vanetza::btp::UpPacket>) override;
+		Facilities& getFacilities();
+		port_type getPortNumber() const;
+		cModule* findHost();
+
+	private:
+		Facilities* m_facilities;
+		ItsG5Middleware* m_middleware;
+};
+
+#endif /* ITSG5BASESERVICE_H_ */
