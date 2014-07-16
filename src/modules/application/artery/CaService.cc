0c20da0b71da287651154da2e50d41c765405ecf
Raphael Riebl <raphael.riebl@thi.de>
2014-07-16 13:28:09 +0200

    artery: CaService

---
+++ src/modules/application/artery/CaService.cc
@@ -0,0 +1,192 @@
+#include "CaService.h"
+#include "Coord.h"
+#include "VehicleDataProvider.h"
+#undef ev
+#include <vanetza/btp/ports.hpp>
+#include <boost/units/cmath.hpp>
+#include <boost/units/systems/si/prefixes.hpp>
+
+auto microdegree = vanetza::units::degree * boost::units::si::micro;
+auto decidegree = vanetza::units::degree * boost::units::si::deci;
+auto centimeter_per_second = vanetza::units::si::meter_per_second * boost::units::si::centi;
+
+
+Define_Module(CaService);
+
+
+bool checkHeadingDelta(vanetza::units::Angle& prev, vanetza::units::Angle now)
+{
+	static const vanetza::units::Angle scHeadingDelta { 4.0 * vanetza::units::degree };
+	return abs(prev - now) > scHeadingDelta;
+}
+
+bool checkPositionDelta(const Coord& prev, const Coord& now)
+{
+	static const double scPositionDeltaMeter = 4.0;
+	return (prev.distance(now) > scPositionDeltaMeter);
+}
+
+bool checkSpeedDelta(vanetza::units::Velocity prev, vanetza::units::Velocity now)
+{
+	static const vanetza::units::Velocity scSpeedDelta = 0.5 * vanetza::units::si::meter_per_second;
+	return abs(prev - now) > scSpeedDelta;
+}
+
+
+CaService::CaService() :
+		mGenCamMin { 100, SIMTIME_MS },
+		mGenCamMax { 1000, SIMTIME_MS },
+		mGenCam(mGenCamMax),
+		mGenCamLowDynamicsCounter(0),
+		mGenCamLowDynamicsLimit(3)
+{
+}
+
+void CaService::trigger()
+{
+	checkTriggeringConditions(getFacilities().getVehicleDataProvider(), simTime());
+}
+
+void CaService::indicate(const vanetza::btp::DataIndication& ind,
+        std::unique_ptr<vanetza::btp::UpPacket> packet)
+{
+    // TODO: collect statistic data
+}
+
+void CaService::checkTriggeringConditions(const VehicleDataProvider& vdp, const simtime_t& T_now)
+{
+	simtime_t& T_GenCam = mGenCam;
+	const simtime_t& T_GenCamMin = mGenCamMin;
+	const simtime_t& T_GenCamMax = mGenCamMax;
+	const simtime_t T_GenCamDcc = genCamDcc();
+	const simtime_t T_elapsed = T_now - mLastCamTimestamp;
+
+	if (T_GenCamDcc < T_GenCamMin || T_GenCamDcc > T_GenCamMax) {
+		opp_error("T_GenCamDcc is out of bounds");
+	}
+
+	if (T_elapsed >= T_GenCamDcc) {
+		if (checkHeadingDelta(mLastCamHeading, vdp.heading()) ||
+			checkPositionDelta(mLastCamPosition, vdp.position()) ||
+			checkSpeedDelta(mLastCamSpeed, vdp.speed())) {
+			sendCam(vdp, T_now);
+			T_GenCam = T_elapsed;
+			mGenCamLowDynamicsCounter = 0;
+		} else if (T_elapsed >= T_GenCam) {
+			sendCam(vdp, T_now);
+			if (++mGenCamLowDynamicsCounter >= mGenCamLowDynamicsLimit) {
+				T_GenCam = T_GenCamMax;
+			}
+		}
+	}
+}
+
+void CaService::sendCam(const VehicleDataProvider& vdp, const simtime_t& T_now)
+{
+	auto cam = createCooperativeAwarenessMessage(vdp);
+	mLastCamPosition = vdp.position();
+	mLastCamSpeed = vdp.speed();
+	mLastCamHeading = vdp.heading();
+	mLastCamTimestamp = T_now;
+	if (T_now - mLastLowCamTimestamp >= simtime_t { 500, SIMTIME_MS }) {
+		addLowFrequencyContainer(cam);
+		mLastLowCamTimestamp = T_now;
+	}
+
+	using namespace vanetza;
+	btp::DataRequestB request;
+	request.destination_port = btp::ports::CAM;
+	request.gn.transport_type = geonet::TransportType::SHB;
+	request.gn.traffic_class.tc_id(static_cast<unsigned>(dcc::Profile::DP2));
+	request.gn.communication_profile = geonet::CommunicationProfile::ITS_G5;
+
+	std::unique_ptr<geonet::DownPacket> payload { new geonet::DownPacket };
+	payload->layer(OsiLayer::Application) = std::move(cam);
+	this->request(request, std::move(payload));
+}
+
+simtime_t CaService::genCamDcc()
+{
+	vanetza::clock::duration delay = getFacilities().getDccScheduler().delay(vanetza::dcc::Profile::DP2);
+	simtime_t dcc { static_cast<int64_t>(delay.count()), SIMTIME_MS };
+	return std::max(mGenCamMin, dcc);
+}
+
+vanetza::asn1::Cam createCooperativeAwarenessMessage(const VehicleDataProvider& vdp)
+{
+	vanetza::asn1::Cam message;
+
+	ItsPduHeader_t& header = (*message).header;
+	header.protocolVersion = ItsPduHeader__protocolVersion_currentVersion;
+	header.messageID = ItsPduHeader__messageID_cam;
+	header.stationID = vdp.station_id();
+
+	CoopAwareness_t& cam = (*message).cam;
+	cam.generationDeltaTime = vdp.timestamp() * GenerationDeltaTime_oneMilliSec;
+	BasicContainer_t& basic = cam.camParameters.basicContainer;
+	HighFrequencyContainer_t& hfc = cam.camParameters.highFrequencyContainer;
+
+	basic.stationType = StationType_passengerCar;
+	basic.referencePosition.altitude.altitudeValue = AltitudeValue_seaLevel;
+	basic.referencePosition.altitude.altitudeConfidence = AltitudeConfidence_unavailable;
+	basic.referencePosition.longitude = (vdp.longitude() / microdegree).value() * Longitude_oneMicrodegreeEast;
+	basic.referencePosition.latitude = (vdp.latitude() / microdegree).value() * Latitude_oneMicrodegreeNorth;
+	basic.referencePosition.positionConfidenceEllipse.semiMajorOrientation.
+	headingValue = HeadingValue_unavailable;
+	basic.referencePosition.positionConfidenceEllipse.semiMajorOrientation.
+	headingConfidence = HeadingConfidence_unavailable;
+	basic.referencePosition.positionConfidenceEllipse.semiMajorConfidence =
+			SemiAxisLength_unavailable;
+	basic.referencePosition.positionConfidenceEllipse.semiMinorConfidence =
+			SemiAxisLength_unavailable;
+
+	hfc.present = HighFrequencyContainer_PR_basicVehicleContainerHighFrequency;
+	BasicVehicleContainerHighFrequency& bvc = hfc.choice.basicVehicleContainerHighFrequency;
+	bvc.heading.headingValue = (vdp.heading() / decidegree).value();
+	bvc.heading.headingConfidence = HeadingConfidence_withinOneDegree;
+	bvc.speed.speedValue = std::abs((vdp.speed() / centimeter_per_second).value()) *
+			SpeedValue_oneCentimeterPerSec;
+	bvc.speed.speedConfidence = SpeedConfidence_withinOneCentimeterPerSec * 3;
+	bvc.driveDirection = vdp.speed().value() >= 0.0 ?
+			DriveDirection_forward : DriveDirection_backward;
+	bvc.longitudinalAcceleration.longitudinalAccelerationValue =
+			(vdp.acceleration() / vanetza::units::si::meter_per_second_squared).value() * LongitudinalAccelerationValue_pointOneMeterPerSecSquaredForward;
+	bvc.longitudinalAcceleration.longitudinalAccelerationConfidence =
+			AccelerationConfidence_unavailable;
+	bvc.curvature.curvatureValue = vdp.curvature() *
+			CurvatureValue_reciprocalOf1MeterRadiusToLeft;
+	bvc.curvature.curvatureConfidence = CurvatureConfidence_unavailable;
+	bvc.curvatureCalculationMode = CurvatureCalculationMode_yawRateUsed;
+	bvc.yawRate.yawRateValue = vdp.yaw_rate() *
+			YawRateValue_degSec_000_01ToLeft * 100.0;
+	bvc.vehicleLength.vehicleLengthValue = VehicleLengthValue_unavailable;
+	bvc.vehicleLength.vehicleLengthConfidenceIndication =
+			VehicleLengthConfidenceIndication_noTrailerPresent;
+	bvc.vehicleWidth = VehicleWidth_unavailable;
+
+	std::string error;
+	if (!message.validate(error)) {
+		opp_error("Invalid High Frequency CAM: %s", error.c_str());
+	}
+
+	return message;
+}
+
+void addLowFrequencyContainer(vanetza::asn1::Cam& message)
+{
+	LowFrequencyContainer_t*& lfc = message->cam.camParameters.lowFrequencyContainer;
+	lfc = vanetza::asn1::allocate<LowFrequencyContainer_t>();
+	lfc->present = LowFrequencyContainer_PR_basicVehicleContainerLowFrequency;
+	BasicVehicleContainerLowFrequency& bvc = lfc->choice.basicVehicleContainerLowFrequency;
+	bvc.vehicleRole = VehicleRole_default;
+	bvc.exteriorLights.buf = static_cast<uint8_t*>(malloc(1));
+	assert(nullptr != bvc.exteriorLights.buf);
+	bvc.exteriorLights.size = 1;
+	bvc.exteriorLights.buf[0] |= 1 << (7 - ExteriorLights_daytimeRunningLightsOn);
+	// TODO: add pathHistory
+
+	std::string error;
+	if (!message.validate(error)) {
+		opp_error("Invalid Low Frequency CAM: %s", error.c_str());
+	}
+}
---
+++ src/modules/application/artery/CaService.h
@@ -0,0 +1,58 @@
+//
+// Copyright (C) 2014 Raphael Riebl <raphael.riebl@thi.de>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+
+#ifndef CASERVICE_H_
+#define CASERVICE_H_
+
+#include "Coord.h"
+#include "ItsG5BaseService.h"
+#include <vanetza/asn1/cam.hpp>
+#include <vanetza/btp/data_interface.hpp>
+#include <vanetza/units/angle.hpp>
+#include <vanetza/units/velocity.hpp>
+#include <simtime_t.h>
+
+
+class CaService : public ItsG5BaseService
+{
+	public:
+		CaService();
+		void indicate(const vanetza::btp::DataIndication&, std::unique_ptr<vanetza::btp::UpPacket>) override;
+		void trigger() override;
+
+	private:
+		void checkTriggeringConditions(const VehicleDataProvider&, const simtime_t&);
+		void sendCam(const VehicleDataProvider&, const simtime_t&);
+		simtime_t genCamDcc();
+
+		const simtime_t mGenCamMin;
+		const simtime_t mGenCamMax;
+		simtime_t mGenCam;
+		unsigned mGenCamLowDynamicsCounter;
+		unsigned mGenCamLowDynamicsLimit;
+		Coord mLastCamPosition;
+		vanetza::units::Velocity mLastCamSpeed;
+		vanetza::units::Angle mLastCamHeading;
+		simtime_t mLastCamTimestamp;
+		simtime_t mLastLowCamTimestamp;
+};
+
+vanetza::asn1::Cam createCooperativeAwarenessMessage(const VehicleDataProvider&);
+void addLowFrequencyContainer(vanetza::asn1::Cam&);
+
+#endif /*CASERVICE_H_ */
