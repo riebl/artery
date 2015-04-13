4a36ccf54a7966b107e3f87ab331af1938865c24
Raphael Riebl <raphael.riebl@thi.de>
2015-04-13 15:55:22 +0200

    application: add enum with DENM cause codes
    Change-Id: I21c7f24c9283a8739f1fe46d7b7be63f07f8cdd1

--- src/artery/application/DenmObject.h
+++ src/artery/application/DenmObject.h
@@ -19,6 +19,37 @@
 #include <cobject.h>
 #include <vanetza/asn1/denm.hpp>
 
+namespace denm {
+
+enum class CauseCode {
+    TrafficCondition = 1,
+    Accident = 2,
+    Roadworks = 3,
+    AdverseWeatherCondition_Adhesion = 6,
+    AdverseWeatherCondition_ExtremeWeather = 17,
+    AdverseWeatherCondition_Visibility = 18,
+    AdverseWeatherCondition_Precipitation = 19,
+    HazardousLocation_SurfaceCondition = 9,
+    HazardousLocation_ObstacleOnTheroad = 10,
+    HazardousLocation_AnimalOnTheRoad = 11,
+    HazardousLocation_DangerousCurve = 96,
+    HumanPresenceOnTheRoad = 12,
+    HumanProblem = 93,
+    RescueAndRecoveryWorkInProgress = 15,
+    EmergencyVehicleApproaching = 95,
+    DangerousEndOfQueue = 27,
+    SlowVehicle = 26,
+    StationaryVehicle = 94,
+    VehicleBreakdown = 91,
+    PostCrash = 92,
+    WrongWayDriving = 14,
+    SignalViolation = 98,
+    CollisionRisk = 97,
+    DangerousSituation = 99
+};
+
+} // namespace denm
+
 class DenmObject : public cObject, public vanetza::asn1::Denm
 {
     public:
