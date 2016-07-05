82b8d5accee2dbcc0dc5195fb44d9a247eb8daaa
obermaierc <if2287@thi.de>
2016-07-05 16:15:07 +0200

    artery: storyboard: use Boost.Geometry for polygon condition check
    Previous implementation contains some subtle bugs.
    
    Change-Id: I4e1904e940b1c40fd1368463cf60fd7cc1f2d2b1

--- src/artery/storyboard/PolygonCondition.cc
+++ src/artery/storyboard/PolygonCondition.cc
@@ -1,16 +1,18 @@
 #include "artery/storyboard/PolygonCondition.h"
 
+#undef ev
+#include <boost/geometry.hpp>
+#include <boost/geometry/geometries/point_xy.hpp>
+#include <boost/geometry/geometries/register/point.hpp>
+
+BOOST_GEOMETRY_REGISTER_POINT_2D(Coord, double, cs::cartesian, x, y)
+
 bool PolygonCondition::testCondition(const Vehicle& car)
 {
     const Coord& point = car.mobility.getCurrentPosition();
-    bool c = false;
-    // Raycasting algorithm, performing the check if the point is inside the polygon
-    for (unsigned i = 0, j = m_vertices.size() - 1; i < m_vertices.size(); j = i++) {
-        if (((m_vertices[i].y > point.y) != (m_vertices[j].y > point.y)) &&
-            (point.y < (m_vertices[j].x - m_vertices[i].x) * (point.y - m_vertices[i].y) / (m_vertices[j].y - m_vertices[i].y) + m_vertices[i].x))
-            c = !c;
-    }
-    return c;
+    boost::geometry::model::polygon<Coord, true, false> polygon1;
+    boost::geometry::assign_points(polygon1, m_vertices);
+    return boost::geometry::within(point, polygon1);
 }
 
 int PolygonCondition::edges() const {
