ab3e2b98188ae672f02aaaaf1532f6e5183ec9a6
Raphael Riebl <raphael.riebl@thi.de>
2016-07-10 11:14:06 +0200

    traci: derive TraCIScenarioManagerArtery from *Base
    Change-Id: I437e30f2ce1fef1637e1ea33d67bf22a53ddecc2

--- src/artery/traci/TraCIScenarioManagerArtery.cc
+++ src/artery/traci/TraCIScenarioManagerArtery.cc
@@ -1,26 +1,41 @@
-#include "artery/traci/TraCIScenarioManagerArtery.h"
 #include "artery/traci/TraCIArteryNodeManager.h"
+#include "artery/traci/TraCISimpleNodeManager.h"
+#include "artery/traci/TraCIScenarioManagerArtery.h"
+#include "veins/modules/mobility/traci/TraCIScenarioManagerLaunchd.h"
+#include <cassert>
 
 Define_Module(TraCIScenarioManagerArtery)
 
 const simsignal_t TraCIScenarioManagerArtery::signalUpdateStep = cComponent::registerSignal("traci.update_step");
 
 TraCIScenarioManagerArtery::TraCIScenarioManagerArtery() :
-    nodes(nullptr)
+    m_launch_config(nullptr), m_seed(-1)
 {
 }
 
 TraCIScenarioManagerArtery::~TraCIScenarioManagerArtery()
 {
-    delete(nodes);
 }
 
 void TraCIScenarioManagerArtery::initialize(int stage)
 {
     parent_class::initialize(stage);
     if (stage == INIT_BASE) {
-        nodes = new TraCIArteryNodeManager(parent_class::getNodeManager(), this);
-        parent_class::setNodeManager(nodes);
+        const std::string moduleTypes = par("moduleType").stdstringValue();
+        const std::string moduleNames = par("moduleName").stdstringValue();
+        const std::string moduleDisplayStrings = par("moduleDisplayString").stdstringValue();
+        m_mapper.parseConfig(moduleTypes, moduleNames, moduleDisplayStrings);
+
+        std::unique_ptr<TraCISimpleNodeManager> nodes_base { new TraCISimpleNodeManager(m_mapper, getParentModule()) };
+        nodes_base->setMobilityModule(par("mobilityNodeModule"));
+        m_nodes_base = std::move(nodes_base);
+        m_nodes.reset(new TraCIArteryNodeManager(m_nodes_base.get(), this));
+        parent_class::setNodeManager(m_nodes.get());
+
+        m_launch_config = par("launchConfig").xmlValue();
+        m_seed = par("seed");
+        Veins::TraCIScenarioManagerLaunchd::prepareSeed(m_seed);
+        Veins::TraCIScenarioManagerLaunchd::prepareLaunchConfig(m_launch_config, m_seed);
     }
 }
 
@@ -31,4 +46,12 @@ void TraCIScenarioManagerArtery::handleSelfMsg(cMessage* msg)
     if (update) {
         emit(signalUpdateStep, updateInterval);
     }
+}
+
+void TraCIScenarioManagerArtery::init_traci()
+{
+    assert(m_launch_config);
+    Veins::TraCIScenarioManagerLaunchd::checkLaunchdCompatibility(*getCommandInterface());
+    Veins::TraCIScenarioManagerLaunchd::sendFileCommand(*connection, m_launch_config);
+    parent_class::init_traci();
 }
--- src/artery/traci/TraCIScenarioManagerArtery.h
+++ src/artery/traci/TraCIScenarioManagerArtery.h
@@ -1,14 +1,16 @@
 #ifndef ARTERY_TRACISCENARIOMANAGERSTORYBOARD_H_QEMRAYD1
 #define ARTERY_TRACISCENARIOMANAGERSTORYBOARD_H_QEMRAYD1
 
+#include "veins/modules/mobility/traci/TraCIModuleMapper.h"
 #include "veins/modules/mobility/traci/TraCIScenarioManagerLaunchd.h"
+#include <memory>
 
 namespace Veins {
 class TraCIListener;
 class TraCINodeManager;
 }
 
-class TraCIScenarioManagerArtery : public Veins::TraCIScenarioManagerLaunchd
+class TraCIScenarioManagerArtery : public Veins::TraCIScenarioManagerBase
 {
 public:
     TraCIScenarioManagerArtery();
@@ -18,9 +20,16 @@ public:
 
     static const simsignal_t signalUpdateStep;
 
+protected:
+    void init_traci() override;
+
 private:
-    typedef Veins::TraCIScenarioManagerLaunchd parent_class;
-    Veins::TraCINodeManager* nodes;
+    typedef Veins::TraCIScenarioManagerBase parent_class;
+    Veins::TraCIModuleMapper m_mapper;
+    std::unique_ptr<Veins::TraCINodeManager> m_nodes_base;
+    std::unique_ptr<Veins::TraCINodeManager> m_nodes;
+    cXMLElement* m_launch_config;
+    int m_seed;
 };
 
 #endif /* ARTERY_TRACISCENARIOMANAGERSTORYBOARD_H_QEMRAYD1 */
