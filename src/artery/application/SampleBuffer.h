50d40d0d6017d805acea6aab47bbd5ab03fdde91
Raphael Riebl <raphael.riebl@thi.de>
2016-12-06 11:10:54 +0100

    application: adds SampleBuffer
    stores arbitrary samples with timestamps
    
    Change-Id: Ia2a021d09a4f3e4ceb9db7b2e58541b5712bdd38

---
+++ src/artery/application/SampleBuffer.h
@@ -0,0 +1,93 @@
+#ifndef ARTERY_SAMPLEBUFFER_H_QHFTL7Z2
+#define ARTERY_SAMPLEBUFFER_H_QHFTL7Z2
+
+#include <boost/circular_buffer.hpp>
+#include <boost/range/iterator_range_core.hpp>
+#include <omnetpp/simtime.h>
+#include <iterator>
+#include <stdexcept>
+
+template<typename T>
+struct Sample
+{
+    using value_type = T;
+
+    Sample(const T& v, omnetpp::SimTime t) :
+         value(v), timestamp(t) {}
+    Sample(T&& v, omnetpp::SimTime t) :
+         value(std::move(v)), timestamp(t) {}
+
+    T value;
+    omnetpp::SimTime timestamp;
+};
+
+template<typename T>
+class SampleBuffer
+{
+public:
+    using sample_type = T;
+    using value_type = Sample<T>;
+    using buffer_type = boost::circular_buffer<value_type>;
+    using iterator = typename buffer_type::const_iterator;
+
+    struct range : public boost::iterator_range<iterator>
+    {
+        range(iterator begin, iterator end) :
+            boost::iterator_range<iterator>(begin, end)
+        {
+        }
+
+        omnetpp::SimTime duration() const
+        {
+            auto d = omnetpp::SimTime::ZERO;
+            if (size() >= 2) {
+                d = begin()->timestamp - std::prev(end())->timestamp;
+            }
+            return d;
+        }
+    };
+
+    void set_capacity(std::size_t n) { m_buffer.set_capacity(n); }
+    std::size_t size() const { return m_buffer.size(); }
+    bool empty() const { return m_buffer.empty(); }
+    iterator begin() const { return iterator(m_buffer.begin()); }
+    iterator end() const { return iterator(m_buffer.end()); }
+
+    const value_type& latest() const { return m_buffer.front(); }
+
+    range not_before(omnetpp::SimTime limit) const
+    {
+        iterator begin = m_buffer.begin();
+        iterator end = std::lower_bound(m_buffer.begin(), m_buffer.end(), limit,
+                [](const value_type& sample, const omnetpp::SimTime& limit) { return sample.timestamp > limit; });
+        return range(begin, end);
+    }
+
+    void insert(const sample_type& sample, omnetpp::SimTime timestamp)
+    {
+        insert_impl(sample, timestamp);
+    }
+
+    void insert(sample_type&& sample, omnetpp::SimTime timestamp)
+    {
+        insert_impl(std::move(sample), timestamp);
+    }
+
+    void clear() { m_buffer.clear(); }
+
+private:
+    template<typename S>
+    void insert_impl(S&& sample, omnetpp::SimTime timestamp)
+    {
+        if (m_buffer.empty() || m_buffer.front().timestamp < timestamp) {
+            m_buffer.push_front(value_type { std::forward<S>(sample), timestamp });
+        } else {
+            throw std::logic_error("chronological order of samples violated");
+        }
+    }
+
+    buffer_type m_buffer;
+};
+
+#endif /* ARTERY_SAMPLEBUFFER_H_QHFTL7Z2 */
+
---
+++ src/artery/application/SampleBufferAlgorithm.h
@@ -0,0 +1,47 @@
+#ifndef ARTERY_SAMPLEBUFFERALGORITHM_H_HIUNJPAK
+#define ARTERY_SAMPLEBUFFERALGORITHM_H_HIUNJPAK
+
+#include <boost/units/operators.hpp>
+#include <boost/units/quantity.hpp>
+#include <boost/units/systems/si/time.hpp>
+#include <numeric>
+#include <type_traits>
+
+template<typename T>
+boost::units::quantity<boost::units::si::time> duration(const Sample<T>& from, const Sample<T>& to)
+{
+    const omnetpp::SimTime& t_from = from.timestamp;
+    const omnetpp::SimTime& t_to = to.timestamp;
+    return (t_to - t_from).dbl() * boost::units::si::seconds;
+}
+
+template<typename T>
+auto differentiate(const Sample<T>& from, const Sample<T>& to) ->
+boost::units::quantity<
+    typename boost::units::divide_typeof_helper<
+        typename T::unit_type,
+        typename boost::units::si::time
+    >::type
+>
+{
+    return (to.value - from.value) / duration(from, to);
+}
+
+template<
+    typename RANGE,
+    typename SAMPLE = typename boost::range_value<RANGE>::type,
+    typename VALUE = typename SAMPLE::value_type,
+    typename std::enable_if<std::is_same<Sample<VALUE>, SAMPLE>::value>::type* = nullptr>
+VALUE average(RANGE r)
+{
+    VALUE avg;
+    if (r.begin() != r.end()) {
+        avg = std::accumulate(r.begin(), r.end(), avg,
+                [](const VALUE& sum, const SAMPLE& sample) { return sum + sample.value; });
+        avg /= r.size();
+    }
+    return avg;
+}
+
+#endif /* ARTERY_SAMPLEBUFFERALGORITHM_H_HIUNJPAK */
+
