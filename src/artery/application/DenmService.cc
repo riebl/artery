235a33f4568b823d1854ea2be3993d52c56ec388
Raphael Riebl <raphael.riebl@thi.de>
2017-03-17 13:07:49 +0100

    DenmService: update file headers
    Change-Id: I57ac1b13f8230016bc2283f928cfbf761d48e246

--- src/artery/application/DenmService.cc
+++ src/artery/application/DenmService.cc
@@ -1,17 +1,8 @@
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-// 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Lesser General Public License for more details.
-// 
-// You should have received a copy of the GNU Lesser General Public License
-// along with this program.  If not, see http://www.gnu.org/licenses/.
-// 
+/*
+ * Artery V2X Simulation Framework
+ * Copyright 2016-2017 Raphael Riebl
+ * Licensed under GPLv2, see COPYING file for detailed license and warranty terms.
+ */
 
 #include "artery/application/Asn1PacketVisitor.h"
 #include "artery/application/DenmObject.h"
--- src/artery/application/DenmService.h
+++ src/artery/application/DenmService.h
@@ -1,20 +1,11 @@
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-// 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Lesser General Public License for more details.
-// 
-// You should have received a copy of the GNU Lesser General Public License
-// along with this program.  If not, see http://www.gnu.org/licenses/.
-// 
+/*
+ * Artery V2X Simulation Framework
+ * Copyright 2016-2017 Raphael Riebl
+ * Licensed under GPLv2, see COPYING file for detailed license and warranty terms.
+ */
 
-#ifndef __ARTERY_DENMSERVICE_H_
-#define __ARTERY_DENMSERVICE_H_
+#ifndef ARTERY_DENMSERVICE_H_
+#define ARTERY_DENMSERVICE_H_
 
 #include "artery/application/DenmMemory.h"
 #include "artery/application/DenmUseCase.h"
