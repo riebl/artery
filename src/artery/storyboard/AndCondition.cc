e870b5a950914275c7bc164bf8400c5b873126c5
Raphael Riebl <raphael.riebl@thi.de>
2017-03-30 13:32:17 +0200

    storyboard: refactor AndCondition
    Enables lazy evaluation for AndConditions
    
    Change-Id: I81619a74e88b9abaad2257b6a10295ce33218538

--- src/artery/storyboard/AndCondition.cc
+++ src/artery/storyboard/AndCondition.cc
@@ -1,47 +1,51 @@
 #include "artery/storyboard/AndCondition.h"
 
+class AndVisitor : public boost::static_visitor<ConditionResult>
+{
+public:
+    ConditionResult operator()(bool lhs, bool rhs) const {
+        return lhs && rhs;
+    }
+
+    ConditionResult operator()(const std::set<const Vehicle*>& lhs, bool rhs) const {
+        if(!rhs) {
+            return false;
+        } else {
+            return lhs;
+        }
+    }
+
+    ConditionResult operator()(bool lhs, const std::set<const Vehicle*>& rhs) const {
+        if(!lhs) {
+            return false;
+        } else {
+            return rhs;
+        }
+    }
+
+    ConditionResult operator()(std::set<const Vehicle*> lhs, std::set<const Vehicle*> rhs) const {
+         std::set<const Vehicle*> intersect;
+         std::set_intersection(lhs.begin(), lhs.end(), rhs.begin(), rhs.end(),
+                 std::inserter(intersect, intersect.begin()));
+         return intersect;
+    }
+};
+
+
 AndCondition::AndCondition(Condition* left, Condition* right) :
     m_left(left), m_right(right)
 {
 }
 
-ConditionResult AndCondition::ResultVisitor::operator()(bool lhs, bool rhs) const
-{
-    return (lhs && rhs);
-}
-
-ConditionResult AndCondition::ResultVisitor::operator()(std::set<const Vehicle*> lhs, bool rhs) const
-{
-    if(!rhs) {
-        return false;
-    } else {
-        return lhs;
-    }
-}
-
-ConditionResult AndCondition::ResultVisitor::operator()(bool lhs, std::set<const Vehicle*> rhs) const
-{
-    if(!lhs) {
-        return false;
-    } else {
-        return rhs;
-    }
-}
-
-ConditionResult AndCondition::ResultVisitor::operator()(std::set<const Vehicle*> lhs, std::set<const Vehicle*> rhs) const
-{
-     std::set<const Vehicle*> intersect;
-     std::set_intersection(lhs.begin(), lhs.end(), rhs.begin(), rhs.end(), std::inserter(intersect, intersect.begin()));
-     return intersect;
-}
-
-
 ConditionResult AndCondition::testCondition(const Vehicle& car)
 {
     ConditionResult lhs = m_left->testCondition(car);
-    ConditionResult rhs = m_right->testCondition(car);
-
-    return boost::apply_visitor(AndCondition::ResultVisitor(), lhs, rhs);
+    if (is_true(lhs)) {
+        ConditionResult rhs = m_right->testCondition(car);
+        return boost::apply_visitor(AndVisitor(), lhs, rhs);
+    } else {
+        return lhs;
+    }
 }
 
 void AndCondition::drawCondition(omnetpp::cCanvas* canvas)
