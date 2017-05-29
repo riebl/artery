6edfac3ce216ab6f97a371ffd8003b572e9cd0fa
Raphael Riebl <raphael.riebl@thi.de>
2017-05-29 19:37:57 +0200

    git subrepo pull --remote=https://github.com/riebl/inet --branch=artery extern/inet
    subrepo:
      subdir:   "extern/inet"
      merged:   "786083fd8"
    upstream:
      origin:   "https://github.com/riebl/inet"
      branch:   "artery"
      commit:   "786083fd8"
    git-subrepo:
      version:  "0.3.1"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "a7ee886"
    
    Change-Id: I62d7e91522174ff6ccaf0d85f33435feded6a752

--- extern/inet/src/inet/physicallayer/common/packetlevel/RadioMedium.cc
+++ extern/inet/src/inet/physicallayer/common/packetlevel/RadioMedium.cc
@@ -425,7 +425,7 @@ const IReceptionResult *RadioMedium::getReceptionResult(const IRadio *radio, con
 
 void RadioMedium::addRadio(const IRadio *radio)
 {
-    radios.push_back(radio);
+    radios.insert(radio);
     communicationCache->addRadio(radio);
     if (neighborCache)
         neighborCache->addRadio(radio);
@@ -448,13 +448,7 @@ void RadioMedium::addRadio(const IRadio *radio)
 
 void RadioMedium::removeRadio(const IRadio *radio)
 {
-    int radioIndex = radio->getId() - radios[0]->getId();
-    radios[radioIndex] = nullptr;
-    int radioCount = 0;
-    while (radios[radioCount] == nullptr && radioCount < (int)radios.size())
-        radioCount++;
-    if (radioCount != 0)
-        radios.erase(radios.begin(), radios.begin() + radioCount);
+    radios.erase(radio);
     communicationCache->removeRadio(radio);
     if (neighborCache)
         neighborCache->removeRadio(radio);
@@ -467,6 +461,11 @@ void RadioMedium::removeRadio(const IRadio *radio)
     if (macAddressFilter)
         getContainingNode(radioModule)->unsubscribe(NF_INTERFACE_CONFIG_CHANGED, this);
     fireRadioRemoved(radio);
+}
+
+bool RadioMedium::hasRadio(const IRadio *radio) const
+{
+    return radios.find(radio) != radios.end();
 }
 
 void RadioMedium::addTransmission(const IRadio *transmitterRadio, const ITransmission *transmission)
