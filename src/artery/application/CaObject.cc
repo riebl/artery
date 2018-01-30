43ac6f890e79e9143b27ff5e80527b7910e44a48
Raphael Riebl <raphael.riebl@thi.de>
2018-01-30 11:44:00 +0100

    CaObject: add camStationId and camGenerationTimeDelta result filters
    Change-Id: Id58a51f805db209ccbafa70ae941e81ac025df71

--- src/artery/application/CaObject.cc
+++ src/artery/application/CaObject.cc
@@ -56,3 +56,35 @@ const vanetza::asn1::Cam& CaObject::asn1() const
 {
     return *m_cam_wrapper;
 }
+
+
+using namespace omnetpp;
+
+class CamStationIdResultFilter : public cObjectResultFilter
+{
+protected:
+    void receiveSignal(cResultFilter* prev, simtime_t_cref t, cObject* object, cObject* details) override
+    {
+        if (auto cam = dynamic_cast<CaObject*>(object)) {
+            const auto id = cam->asn1()->header.stationID;
+            fire(this, t, id, details);
+        }
+    }
+};
+
+Register_ResultFilter("camStationId", CamStationIdResultFilter)
+
+
+class CamGenerationDeltaTimeResultFilter : public cObjectResultFilter
+{
+protected:
+    void receiveSignal(cResultFilter* prev, simtime_t_cref t, cObject* object, cObject* details) override
+    {
+        if (auto cam = dynamic_cast<CaObject*>(object)) {
+            const auto genDeltaTime = cam->asn1()->cam.generationDeltaTime;
+            fire(this, t, genDeltaTime, details);
+        }
+    }
+};
+
+Register_ResultFilter("camGenerationDeltaTime", CamGenerationDeltaTimeResultFilter)
