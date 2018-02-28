417d1fe58f90a0defcfe94a429347086a07f73de
Christian Hagau <hagau@ibr.cs.tu-bs.de>
2018-02-28 00:00:00 +0000

    envmod: Add CarIdentityRegistrant for registering a Car without V2X in the IdentityRegistry

---
+++ src/artery/envmod/CarIdentityRegistrant.cc
@@ -0,0 +1,44 @@
+#include "artery/envmod/CarIdentityRegistrant.h"
+#include "artery/utility/IdentityRegistry.h"
+#include "artery/traci/MobilityBase.h"
+
+
+namespace artery
+{
+
+Define_Module(CarIdentityRegistrant)
+
+void CarIdentityRegistrant::initialize()
+{
+	initializeIdentity();
+	registerIdentity();
+}
+
+void CarIdentityRegistrant::initializeIdentity()
+{
+	auto parent = this->getParentModule();
+	auto mobility = dynamic_cast<MobilityBase*>(parent->getSubmodule("mobility", -1));
+	if (!mobility) {
+		throw omnetpp::cRuntimeError("no suitable mobility module found");
+	}
+	auto controller = mobility->getVehicleController();
+
+	auto traciId = controller->getVehicleId();
+	auto stationId = omnetpp::intuniform(getRNG(0), 0, 4294967295);
+
+	mIdentity.application = stationId;
+	mIdentity.traci = traciId;
+	mIdentity.geonet = vanetza::geonet::Address();
+}
+
+void CarIdentityRegistrant::registerIdentity()
+{
+	emit(artery::IdentityRegistry::updateSignal, &mIdentity);
+}
+
+void CarIdentityRegistrant::finish()
+{
+	emit(artery::IdentityRegistry::removeSignal, &mIdentity);
+}
+
+} // namespace artery
