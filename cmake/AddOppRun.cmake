68afaab365a56a434429041a9578403c7fe83f8e
Raphael Riebl <raphael.riebl@thi.de>
2018-03-18 21:45:02 +0100

    cmake: add_opp_run: fix look up of opp_run for debug builds with OMNeT++ 5.1
    Change-Id: I3b3d2abf8cf33a9c31ace6893e01e23e33709c9a

--- cmake/AddOppRun.cmake
+++ cmake/AddOppRun.cmake
@@ -1,5 +1,5 @@
-find_program(OMNETPP_RUN NAMES opp_run opp_run_release PATHS ${OMNETPP_ROOT}/bin DOC "OMNeT++ opp_run executable")
-find_program(OMNETPP_RUN_DEBUG NAMES opp_run_dbg PATHS ${OMNETPP_ROOT}/bin DOC "OMNeT++ opp_run_dbg executable")
+find_program(OMNETPP_RUN NAMES opp_run_release opp_run PATHS ${OMNETPP_ROOT}/bin DOC "OMNeT++ opp_run executable")
+find_program(OMNETPP_RUN_DEBUG NAMES opp_run_dbg opp_run PATHS ${OMNETPP_ROOT}/bin DOC "OMNeT++ opp_run_dbg executable")
 include(CMakeParseArguments)
 
 function(_add_opp_run_libraries _target _output)
