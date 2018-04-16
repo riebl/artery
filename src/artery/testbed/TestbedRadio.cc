86886c66e46b713b70c00410d949d372247b609f
Christina <christina.obermaier@thi.de>
2018-04-16 11:09:32 +0200

    testbed: adds TestbedRadio
    Change-Id: I628a9c4b6253d563f1e40cc74901315734d6467d

---
+++ src/artery/testbed/TestbedRadio.cc
@@ -0,0 +1,47 @@
+#include "artery/testbed/TestbedRadio.h"
+#include <inet/linklayer/ieee80211/mac/Ieee80211Frame_m.h>
+#include <inet/physicallayer/common/packetlevel/RadioMedium.h>
+#include <inet/physicallayer/common/packetlevel/Radio.h>
+
+namespace artery
+{
+
+Define_Module(TestbedRadio)
+
+void TestbedRadio::initialize(int stage)
+{
+    Ieee80211Radio::initialize(stage);
+    mReachableTime = par("reachableTime");
+}
+
+void TestbedRadio::endReception(cMessage *timer)
+{
+    auto radioFrame = static_cast<inet::physicallayer::RadioFrame*>(timer->getControlInfo());
+    auto transmission = radioFrame->getTransmission();
+    auto macFrame = dynamic_cast<const inet::ieee80211::Ieee80211DataOrMgmtFrame*>(transmission->getMacFrame());
+    auto part = static_cast<inet::physicallayer::IRadioSignal::SignalPart>(timer->getKind());
+    auto receptionDecision = getMedium()->getReceptionDecision(this, radioFrame->getListening(), transmission, part);
+    if (receptionDecision->isReceptionSuccessful() && macFrame) {
+        mReachableNodes[macFrame->getTransmitterAddress()] = simTime();
+    }
+    Radio::endReception(timer);
+}
+
+void TestbedRadio::updateReachableNodes()
+{
+    for (auto reachableNode = mReachableNodes.begin(); reachableNode != mReachableNodes.end();) {
+        if ((simTime() - reachableNode->second) > mReachableTime) {
+            reachableNode = mReachableNodes.erase(reachableNode);
+        } else {
+            ++reachableNode;
+        }
+    }
+}
+
+const std::map<inet::MACAddress, omnetpp::simtime_t>& TestbedRadio::getReachableNodes()
+{
+    updateReachableNodes();
+    return mReachableNodes;
+}
+
+} // namespace artery
---
+++ src/artery/testbed/TestbedRadio.h
@@ -0,0 +1,34 @@
+/*
+ * Artery V2X Simulation Framework
+ * Copyright 2017-2018 Christina Obermaier
+ * Licensed under GPLv2, see COPYING file for detailed license and warranty terms.
+ */
+
+#ifndef ARTERY_TESTBEDRADIO_H
+#define ARTERY_TESTBEDRADIO_H
+
+#include <inet/linklayer/common/MACAddress.h>
+#include <inet/physicallayer/ieee80211/packetlevel/Ieee80211Radio.h>
+#include <map>
+
+namespace artery
+{
+
+class TestbedRadio : public inet::physicallayer::Ieee80211Radio
+{
+    public:
+        virtual const std::map<inet::MACAddress, omnetpp::simtime_t>& getReachableNodes();
+
+    protected:
+        void initialize(int stage) override;
+        void endReception(cMessage *timer) override;
+        virtual void updateReachableNodes();
+
+    private:
+        std::map<inet::MACAddress, omnetpp::simtime_t> mReachableNodes;
+        omnetpp::simtime_t mReachableTime;
+};
+
+} // namespace artery
+
+#endif /* ARTERY_TESTBEDRADIO_H */
