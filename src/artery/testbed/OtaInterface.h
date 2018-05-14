6f7fdb19a4d14d63d9a41ca4313ae0522f075f59
Christina <christina.obermaier@thi.de>
2018-05-14 14:22:24 +0200

    testbed: adds comments to OtaInterface
    Change-Id: I8ddc73b26878f6968c9dc12309f56222354983c7

--- src/artery/testbed/OtaInterface.h
+++ src/artery/testbed/OtaInterface.h
@@ -17,13 +17,46 @@ namespace artery
 
 class OtaInterfaceLayer;
 
+/**
+ * Interface module for all OtaInterface classes.
+ * The OtaInterface is a global OMNeT++ Module which maintains the data exchange between a certain DUT and the interface hardware
+ */
 class OtaInterface
 {
 public:
+    /**
+     * Registers a OtaInterfaceLayer at the OtaInterface
+     *
+     * \param OtaInterfaceLayer to register
+     */
     virtual void registerModule(OtaInterfaceLayer*) = 0;
+
+    /**
+     * Unregisters an allready registered OtaInterfaceLayer
+     */
     virtual void unregisterModule() = 0;
+
+    /**
+     * Transmits a simulated packet to the DUT
+     *
+     * \param cModule which is the sender (usually an OtaInterfaceLayer)
+     * \param MacAddress source MAC address (usually the MAC of a simulated vehicle)
+     * \param MacAddress destination MAC address
+     * \param byte_view_range containing the message data
+     */
     virtual void sendMessage(const vanetza::MacAddress& source, const vanetza::MacAddress& destination, const vanetza::byte_view_range& data) = 0;
+
+    /**
+     * Receives a GeonetPacket which was scheduled by the used scheduler
+     * The packet should be transmitted using a OMNeT++ module (eg. the registered OtaInterfaceLayer)
+     *
+     * \param GeonetPacket which should be sent to the simulation
+     */
     virtual void receiveMessage(std::unique_ptr<GeoNetPacket>) = 0;
+
+    /**
+     * Should check if a module was registered at the OtaInterface
+     */
     virtual bool hasRegisteredModule() = 0;
 
     virtual ~OtaInterface() = default;
