95988da6ee401bc6f6830095157189f956b0cd7f
Raphael Riebl <raphael.riebl@thi.de>
2018-05-29 11:30:36 +0200

    traci: add variable trait
    Change-Id: I6312c94c53a2ddb9233c90707eed55b800a1d64c

---
+++ src/traci/VariableTraits.h
@@ -0,0 +1,38 @@
+/*
+ * Artery V2X Simulation Framework
+ * Copyright 2018 Raphael Riebl
+ * Licensed under GPLv2, see COPYING file for detailed license and warranty terms.
+ */
+
+#ifndef VARIABLETRAITS_H_LZ4RYGAV
+#define VARIABLETRAITS_H_LZ4RYGAV
+
+#include "traci/sumo/traci-server/TraCIConstants.h"
+
+namespace traci
+{
+
+template<int VAR>
+struct VariableTrait;
+
+#define VAR_TRAIT(var_, type_) \
+    template<> struct VariableTrait<var_> { using value_type = type_; };
+
+VAR_TRAIT(VAR_SPEED, double)
+VAR_TRAIT(VAR_POSITION, TraCIPosition)
+VAR_TRAIT(VAR_ANGLE, double)
+VAR_TRAIT(VAR_MAXSPEED, double)
+VAR_TRAIT(VAR_TYPE, std::string)
+VAR_TRAIT(VAR_VEHICLECLASS, std::string)
+VAR_TRAIT(VAR_LENGTH, double)
+VAR_TRAIT(VAR_WIDTH, double)
+VAR_TRAIT(VAR_ARRIVED_VEHICLES_IDS, std::vector<std::string>)
+VAR_TRAIT(VAR_DEPARTED_VEHICLES_IDS, std::vector<std::string>)
+VAR_TRAIT(VAR_TIME_STEP, SUMOTime)
+
+#undef VAR_TRAIT
+
+} // namespace traci
+
+#endif /* VARIABLETRAITS_H_LZ4RYGAV */
+
