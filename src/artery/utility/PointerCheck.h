ab18d45e41b7e2772b0d1d3ca432b7ece27e9689
Raphael Riebl <raphael.riebl@thi.de>
2018-10-24 14:56:00 +0200

    utility: add notNullPtr checking function
    Change-Id: I232c536bf89cccba84199a5c08fad0b32b3650c6

---
+++ src/artery/utility/PointerCheck.h
@@ -0,0 +1,29 @@
+#ifndef ARTERY_POINTERCHECK_H_U9GMXBHO
+#define ARTERY_POINTERCHECK_H_U9GMXBHO
+
+#include <omnetpp/cexception.h>
+#include <omnetpp/simutil.h>
+#include <memory>
+
+namespace artery
+{
+
+template<typename T>
+T* notNullPtr(T* ptr)
+{
+    if (!ptr) {
+        throw omnetpp::cRuntimeError("%s: nullptr violation", omnetpp::opp_typename(typeid(T)));
+    }
+    return ptr;
+}
+
+template<typename T>
+T* notNullPtr(std::unique_ptr<T>& ptr)
+{
+    return notNullPtr(ptr.get());
+}
+
+} // namespace artery
+
+#endif /* ARTERY_POINTERCHECK_H_U9GMXBHO */
+
