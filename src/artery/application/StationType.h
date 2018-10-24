81cb9169c3c40a4fcae7c0e1f6afe2283178d761
Raphael Riebl <raphael.riebl@thi.de>
2018-10-24 14:56:19 +0200

    application: add StationType cObject
    Change-Id: Id109c9e6d64779eb6f284812319790c4ac195ca1

---
+++ src/artery/application/StationType.h
@@ -0,0 +1,37 @@
+#ifndef ARTERY_STATIONTYPE_H_ORMPZL5X
+#define ARTERY_STATIONTYPE_H_ORMPZL5X
+
+#include <omnetpp/cobject.h>
+#include <vanetza/geonet/station_type.hpp>
+
+namespace artery
+{
+
+class StationType : public omnetpp::cObject
+{
+public:
+    StationType() : m_type(vanetza::geonet::StationType::UNKNOWN) {}
+    StationType(vanetza::geonet::StationType type) : m_type(type) {}
+
+    void setStationType(vanetza::geonet::StationType type) { m_type = type; }
+    vanetza::geonet::StationType getStationType() const { return m_type; }
+
+    inline StationType& operator=(vanetza::geonet::StationType type)
+    {
+        setStationType(type);
+        return *this;
+    }
+
+    inline operator vanetza::geonet::StationType() const
+    {
+        return getStationType();
+    }
+
+private:
+    vanetza::geonet::StationType m_type;
+};
+
+} // namespace artery
+
+#endif /* ARTERY_STATIONTYPEOBJECT_H_ORMPZL5X */
+
