f33d084fd28937da33907406a8277c61a3c94eef
Christian Hagau <hagau@ibr.cs.tu-bs.de>
2019-04-26 00:00:00 +0000

    application: add IndicationInterface
    Change-Id: I5cdd10221b2b70dc753453cb91def2a06d805c5e

---
+++ src/artery/application/IndicationInterface.h
@@ -0,0 +1,24 @@
+#ifndef ARTERY_INDICATIONINTERFACE_H_
+#define ARTERY_INDICATIONINTERFACE_H_
+
+#include <vanetza/btp/data_indication.hpp>
+
+namespace artery
+{
+
+class NetworkInterface;
+
+/**
+ * IndicationInterface has to be implemented by any entity receiving ITS-G5 messages.
+ */
+class IndicationInterface
+{
+    public:
+        virtual void indicate(const vanetza::btp::DataIndication&, std::unique_ptr<vanetza::UpPacket>, const NetworkInterface&) = 0;
+
+        virtual ~IndicationInterface() = default;
+};
+
+} // namespace artery
+
+#endif
