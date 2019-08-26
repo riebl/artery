706d00d24124428bb6c0042df8f26d3be516b37c
Raphael Riebl <raphael.riebl@thi.de>
2019-08-26 13:10:15 +0200

    rsu_grid: adhere to PEP8 in RSUGenerator.py
    Change-Id: Ia49144f2c3cc044fec63bd6b3c177899a3d8222b

--- scenarios/rsu_grid/RSUGenerator.py
+++ scenarios/rsu_grid/RSUGenerator.py
@@ -1,68 +1,70 @@
 #!/usr/bin/env python
+import argparse
+import math
+import numpy
 import os
 import sys
+from lxml import etree
 
 if 'SUMO_HOME' in os.environ:
-	tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
-	sys.path.append(tools)
-else:   
-	sys.exit("please declare environment variable 'SUMO_HOME'")
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path.append(tools)
+else:
+    sys.exit("please declare environment variable 'SUMO_HOME'")
 
-
-import argparse
-import sumolib
-import numpy
-import math
-from lxml import etree
+import sumolib  # noqa
 
 
 parser = argparse.ArgumentParser(
-	description='RSU XML generation script for junctions.',
-	epilog="""This script reads junctions coordinates and their edges from SUMO network XML file. 
-				Generates a RSU in the center of every junction and adds an antenna for each outgoing edge.""")
+    description='RSU XML generation script for junctions.',
+    epilog="""This script reads junctions coordinates and their edges from
+        SUMO network XML file. Generates a RSU in the center of every junction
+        and adds an antenna for each outgoing edge.""")
 
-parser.add_argument('-n',
-    help='SUMO network file to parse', 
-	type=str, metavar='<path>/<scenario>.net.xml', required=True)
+parser.add_argument(
+    '-n', help='SUMO network file to parse',
+    type=str, metavar='<path>/<scenario>.net.xml', required=True)
 
-parser.add_argument('-o',
-    help='File to write RSU definitions to', 
-	type=str, metavar='<path>/RSU.xml', required=True)
+parser.add_argument(
+    '-o', help='File to write RSU definitions to',
+    type=str, metavar='<path>/RSU.xml', required=True)
 
-	
+
 args = parser.parse_args()
-
 net = sumolib.net.readNet(args.n)
-
 junctionCount = 0
 
 root = etree.Element("RSUs")
-
-root.append(etree.Comment("This file was generated with \'" + " ".join(sys.argv) + "\'"))
+root.append(etree.Comment(
+    "This file was generated with '{}'".format(" ".join(sys.argv))))
 
 for junction in net.getNodes():
-	junctionCount += 1
+    junctionCount += 1
 
-	nodePos = junction.getCoord()
+    nodePos = junction.getCoord()
 
-	doc = etree.SubElement(root, "rsu", junctionID=junction.getID() , positionX=str(nodePos[0]), positionY=str(nodePos[1]))
+    doc = etree.SubElement(
+        root, "rsu",
+        junctionID=junction.getID(),
+        positionX=str(nodePos[0]), positionY=str(nodePos[1]))
 
-	for edge in junction.getOutgoing():
-		outgoingNodePos = edge.getToNode().getCoord()
-		directionVector = tuple(numpy.subtract(outgoingNodePos, nodePos))
-		length = numpy.linalg.norm(directionVector)
-		normalizedDirectionVector = tuple(pos/length for pos in directionVector)
-		normalizedDirectionVector = tuple((normalizedDirectionVector[0], normalizedDirectionVector[1] * -1))
-		direction = numpy.arctan2(normalizedDirectionVector[1], normalizedDirectionVector[0])
+    for edge in junction.getOutgoing():
+        outgoingNodePos = edge.getToNode().getCoord()
+        directionVector = numpy.subtract(outgoingNodePos, nodePos)
+        length = numpy.linalg.norm(directionVector)
+        normalizedDirectionVector = [pos / length for pos in directionVector]
+        normalizedDirectionVector = [normalizedDirectionVector[0],
+                                     normalizedDirectionVector[1] * -1]
+        direction = numpy.arctan2(normalizedDirectionVector[1],
+                                  normalizedDirectionVector[0])
 
-		if(direction == math.pi * -1):
-			direction += 2 * math.pi
+        if(direction == math.pi * -1):
+            direction += 2 * math.pi
 
-		if(direction == -0):
-			direction = 0
+        if(direction == -0):
+            direction = 0
 
-		antenna = etree.SubElement(doc, "antenna", direction=str(direction))
+        antenna = etree.SubElement(doc, "antenna", direction=str(direction))
 
-tree = etree.ElementTree(root)
-
-tree.write(args.o, pretty_print=True)
+    tree = etree.ElementTree(root)
+    tree.write(args.o, pretty_print=True)
