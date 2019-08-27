3af9bfd3525c758d6a899a92330c2d46c080bb02
Raphael Riebl <raphael.riebl@thi.de>
2019-08-27 09:01:33 +0200

    StaticNodeManager: restrict visibility of cSimpleModule methods
    Change-Id: I712a61ebd4d719ec7a7e895a44d02272488f01a6

--- src/artery/StaticNodeManager.h
+++ src/artery/StaticNodeManager.h
@@ -15,16 +15,16 @@ class StaticNodeManager : public omnetpp::cSimpleModule
 public:
     static const omnetpp::simsignal_t addRoadSideUnitSignal;
 
-    void initialize(int stage) override;
-    int numInitStages() const override;
-
+protected:
     struct RSU
     {
         Position position;
         std::vector<double> antennaDirections;
     };
 
-protected:
+    void initialize(int stage) override;
+    int numInitStages() const override;
+
     virtual void loadRoadSideUnits();
     virtual void addRoadSideUnit(const std::string& index);
     virtual omnetpp::cModule* createRoadSideUnitModule(const std::string&);
