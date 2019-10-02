243a89bcd8256440abe4e45a334b588c34e6577f
Raphael Riebl <raphael.riebl@thi.de>
2019-10-02 18:14:39 +0200

    envmod: move FieldOfView struct to artery namespace
    Change-Id: Ifdfd44406b0a375f80588eb160ca5481ab31a1aa

--- src/artery/envmod/sensor/FieldOfView.h
+++ src/artery/envmod/sensor/FieldOfView.h
@@ -5,10 +5,15 @@
 #include <boost/units/systems/si/length.hpp>
 #include <boost/units/systems/angle/degrees.hpp>
 
+namespace artery
+{
+
 struct FieldOfView
 {
     boost::units::quantity<boost::units::si::length> range;
     boost::units::quantity<boost::units::degree::plane_angle> angle;
 };
+
+} // namespace artery
 
 #endif /* ENVMOD_FIELDOFVIEW_H_RW4CQUCV */
