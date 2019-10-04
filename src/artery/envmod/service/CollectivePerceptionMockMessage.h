b63b8c8ab7536537d13d81603c8ab7cbee4a9007
Raphael Riebl <raphael.riebl@thi.de>
2019-10-04 13:18:05 +0200

    envmod: add a collective perception mock-up service
    Change-Id: I2d221cc5d358675d6ba1151e8aa9c3fc822c4657

---
+++ src/artery/envmod/service/CollectivePerceptionMockMessage.h
@@ -0,0 +1,54 @@
+#ifndef ARTERY_COLLECTIVEPERCEPTIONMOCKMESSAGE_H_YUFJ5PSV
+#define ARTERY_COLLECTIVEPERCEPTIONMOCKMESSAGE_H_YUFJ5PSV
+
+#include "artery/envmod/LocalEnvironmentModel.h"
+#include "artery/envmod/sensor/FieldOfView.h"
+#include "artery/envmod/sensor/SensorPosition.h"
+#include <omnetpp/cpacket.h>
+#include <memory>
+#include <vector>
+
+namespace artery
+{
+
+class CollectivePerceptionMockMessage : public omnetpp::cPacket
+{
+public:
+    struct FovContainer
+    {
+        int sensorId = 0;
+        SensorPosition position;
+        FieldOfView fov;
+    };
+
+    struct ObjectContainer
+    {
+        omnetpp::SimTime timeOfMeasurement = omnetpp::SimTime::ZERO;
+        int objectId = 0;
+        int sensorId = 0;
+        std::weak_ptr<EnvironmentModelObject> object;
+    };
+
+    CollectivePerceptionMockMessage();
+    CollectivePerceptionMockMessage(const CollectivePerceptionMockMessage&) = default;
+    CollectivePerceptionMockMessage& operator=(const CollectivePerceptionMockMessage&) = default;
+
+    const std::vector<FovContainer>& getFovContainers() const { return *mFovContainers; }
+    void setFovContainers(std::vector<FovContainer>&& fovs);
+    void setFovContainers(const std::vector<FovContainer>& fovs);
+
+    const std::vector<ObjectContainer>& getObjectContainers() const { return *mObjectContainers; }
+    void setObjectContainers(std::vector<ObjectContainer>&& objs);
+    void setObjectContainers(const std::vector<ObjectContainer>& objs);
+
+    omnetpp::cPacket* dup() const override;
+
+private:
+    std::shared_ptr<const std::vector<FovContainer>> mFovContainers;
+    std::shared_ptr<const std::vector<ObjectContainer>> mObjectContainers;
+};
+
+} // namespace artery
+
+#endif /* ARTERY_COLLECTIVEPERCEPTIONMOCKMESSAGE_H_YUFJ5PSV */
+
---
+++ src/artery/envmod/service/CollectivePerceptionMockService.cc
@@ -0,0 +1,148 @@
+/*
+* Artery V2X Simulation Framework
+* Copyright 2019 Raphael Riebl et al.
+* Licensed under GPLv2, see COPYING file for detailed license and warranty terms.
+*/
+
+#include "artery/envmod/sensor/Sensor.h"
+#include "artery/envmod/service/CollectivePerceptionMockMessage.h"
+#include "artery/envmod/service/CollectivePerceptionMockService.h"
+#include "artery/utility/InitStages.h"
+#include <omnetpp/cmessage.h>
+#include <omnetpp/cpacket.h>
+
+namespace artery
+{
+
+Define_Module(CollectivePerceptionMockService)
+
+namespace
+{
+
+omnetpp::simsignal_t camSentSignal = omnetpp::cComponent::registerSignal("CamSent");
+
+} // namespace
+
+
+CollectivePerceptionMockService::~CollectivePerceptionMockService()
+{
+    cancelAndDelete(mTrigger);
+}
+
+int CollectivePerceptionMockService::numInitStages() const
+{
+    return InitStages::Total;
+}
+
+void CollectivePerceptionMockService::initialize(int stage)
+{
+    if (stage == InitStages::Prepare) {
+        ItsG5Service::initialize();
+        mPositionProvider = &getFacilities().get_const<PositionProvider>();
+        mEnvironmentModel = &getFacilities().get_const<LocalEnvironmentModel>();
+
+        mDccProfile = par("dccProfile");
+        mLengthHeader = par("lengthHeader");
+        mLengthFovContainer = par("lengthFovContainer");
+        mLengthObjectContainer = par("lengthObjectContainer");
+
+        mGenerateAfterCam = par("generateAfterCam");
+        mCpmOffset = par("cpmOffset");
+        mCpmInterval = par("cpmInterval");
+    } else if (stage == InitStages::Self) {
+        mTrigger = new omnetpp::cMessage("triggger mock-up CPM");
+        if (mGenerateAfterCam) {
+            subscribe(camSentSignal);
+        }
+    } else if (stage == InitStages::Propagate) {
+        for (const Sensor* sensor : mEnvironmentModel->getSensors()) {
+            // skip any virtual sensors without field of view
+            if (auto fov = sensor->getFieldOfView()) {
+                CollectivePerceptionMockMessage::FovContainer fovContainer;
+                fovContainer.sensorId = sensor->getId();
+                fovContainer.position = sensor->position();
+                fovContainer.fov = *fov;
+                mFovContainers.emplace_back(std::move(fovContainer));
+                mSensors.insert(sensor);
+            }
+        }
+    }
+}
+
+void CollectivePerceptionMockService::handleMessage(omnetpp::cMessage* msg)
+{
+    if (msg == mTrigger) {
+        generatePacket();
+    } else {
+        ItsG5Service::handleMessage(msg);
+    }
+}
+
+void CollectivePerceptionMockService::receiveSignal(omnetpp::cComponent*, omnetpp::simsignal_t signal,
+        omnetpp::cObject* obj, omnetpp::cObject*)
+{
+    if (signal == camSentSignal) {
+        scheduleAt(omnetpp::simTime() + mCpmOffset, mTrigger);
+    }
+}
+
+void CollectivePerceptionMockService::trigger()
+{
+    if (!mGenerateAfterCam && !mTrigger->isScheduled()) {
+        auto channel = getFacilities().get_const<MultiChannelPolicy>().primaryChannel(vanetza::aid::CP);
+        auto netifc = notNullPtr(getFacilities().get_const<NetworkInterfaceTable>().select(channel));
+        vanetza::dcc::TransmitRateThrottle* trc = notNullPtr(netifc->getDccEntity().getTransmitRateThrottle());
+        vanetza::dcc::TransmissionLite tx { static_cast<vanetza::dcc::Profile>(mDccProfile), 0 };
+        const omnetpp::SimTime interval = std::chrono::duration<double>(trc->interval(tx)).count();
+        const omnetpp::SimTime next = omnetpp::simTime() + mCpmOffset;
+        if (mTrigger->getArrivalTime() + std::max(interval, mCpmInterval) <= next) {
+            scheduleAt(next, mTrigger);
+        }
+    }
+}
+
+void CollectivePerceptionMockService::generatePacket()
+{
+    using namespace vanetza;
+    btp::DataRequestB req;
+    req.destination_port = host_cast<PortNumber>(getPortNumber());
+    req.gn.transport_type = geonet::TransportType::SHB;
+    req.gn.traffic_class.tc_id(mDccProfile);
+    req.gn.communication_profile = geonet::CommunicationProfile::ITS_G5;
+
+    auto packet = new CollectivePerceptionMockMessage();
+    packet->setByteLength(mLengthHeader);
+
+    if (mFovLast + mFovInterval <= omnetpp::simTime()) {
+        packet->setFovContainers(mFovContainers);
+        packet->addByteLength(mLengthFovContainer * mFovContainers.size());
+        mFovLast = omnetpp::simTime();
+    }
+
+    std::vector<CollectivePerceptionMockMessage::ObjectContainer> objectContainers;
+    using TrackedObject = LocalEnvironmentModel::TrackedObject;
+    for (const TrackedObject& object : mEnvironmentModel->allObjects()) {
+        const LocalEnvironmentModel::Tracking& tracking = object.second;
+        if (tracking.expired()) {
+            // skip objects with lost tracking
+            continue;
+        }
+
+        for (const auto& sensor : tracking.sensors()) {
+            if (mSensors.find(sensor.first) != mSensors.end()) {
+                CollectivePerceptionMockMessage::ObjectContainer objectContainer;
+                objectContainer.object = object.first;
+                objectContainer.objectId = tracking.id();
+                objectContainer.sensorId = sensor.first->getId();
+                objectContainer.timeOfMeasurement = sensor.second.last();
+                objectContainers.emplace_back(std::move(objectContainer));
+            }
+        }
+    }
+    packet->addByteLength(mLengthObjectContainer * objectContainers.size());
+    packet->setObjectContainers(std::move(objectContainers));
+
+    request(req, packet);
+}
+
+} // namespace artery
