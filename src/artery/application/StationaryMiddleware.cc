f865b0144d6298c705c92c365105fa81ab77b382
Raphael Riebl <raphael@riebl.net>
2020-05-29 17:06:49 +0200

    StationaryMiddleware: add GeoPosition to Facilities

--- src/artery/application/StationaryMiddleware.cc
+++ src/artery/application/StationaryMiddleware.cc
@@ -1,22 +1,44 @@
 /*
  * Artery V2X Simulation Framework
- * Copyright 2014-2018 Raphael Riebl
+ * Copyright 2014-2020 Raphael Riebl
  * Licensed under GPLv2, see COPYING file for detailed license and warranty terms.
  */
 
 #include "artery/application/StationaryMiddleware.h"
+#include "artery/utility/Geometry.h"
+#include "artery/utility/InitStages.h"
+#include <omnetpp/checkandcast.h>
+#include <vanetza/common/position_fix.hpp>
 
 namespace artery
 {
+
+using namespace omnetpp;
+static const simsignal_t scPositionFixSignal = cComponent::registerSignal("PositionFix");
 
 Define_Module(StationaryMiddleware)
 
 void StationaryMiddleware::initialize(int stage)
 {
-    Middleware::initialize(stage);
-
-    if (stage == 0) {
+    if (stage == InitStages::Self) {
         setStationType(vanetza::geonet::StationType::RSU);
+
+        if (cModule* host = findHost()) {
+            // position will be set by PositionFix signal
+            host->subscribe(scPositionFixSignal, this);
+            getFacilities().register_const(&mGeoPosition);
+        }
+    }
+
+    Middleware::initialize(stage);
+}
+
+void StationaryMiddleware::receiveSignal(cComponent*, simsignal_t signal, cObject* obj, cObject*)
+{
+    if (signal == scPositionFixSignal) {
+        auto position = check_and_cast<const vanetza::PositionFix*>(obj);
+        mGeoPosition.latitude = position->latitude;
+        mGeoPosition.longitude = position->longitude;
     }
 }
 
--- src/artery/application/StationaryMiddleware.h
+++ src/artery/application/StationaryMiddleware.h
@@ -1,6 +1,6 @@
 /*
  * Artery V2X Simulation Framework
- * Copyright 2014-2018 Raphael Riebl
+ * Copyright 2014-2020 Raphael Riebl
  * Licensed under GPLv2, see COPYING file for detailed license and warranty terms.
  */
 
@@ -8,6 +8,7 @@
 #define ARTERY_STATIONARYMIDDLEWARE_H_IE9M1YJ3
 
 #include "artery/application/Middleware.h"
+#include "artery/utility/Geometry.h"
 
 namespace artery
 {
@@ -16,6 +17,10 @@ class StationaryMiddleware : public Middleware
 {
     public:
         void initialize(int stage) override;
+        void receiveSignal(omnetpp::cComponent*, omnetpp::simsignal_t, omnetpp::cObject*, omnetpp::cObject*) override;
+
+    private:
+        GeoPosition mGeoPosition;
 };
 
 } // namespace artery
