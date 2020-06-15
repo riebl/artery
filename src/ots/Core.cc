94192b48ec3486c91f711448c64476585143018e
Raphael Riebl <raphael@riebl.net>
2020-06-15 17:33:25 +0200

    ots: migrate protocol to r6499
    Change-Id: If6da8947b37d267065ed9efefdb5587fb663c3a2

--- src/ots/Core.cc
+++ src/ots/Core.cc
@@ -1,5 +1,6 @@
 #include "ots/Core.h"
 #include "ots/GtuObject.h"
+#include <boost/container_hash/hash.hpp> // for hashing sim0mq::Identifier
 #include <omnetpp/cmessage.h>
 #include <sim0mqpp/buffer_serialization.hpp>
 #include <sim0mqpp/quantity.hpp>
@@ -17,11 +18,19 @@ Define_Module(Core)
 namespace
 {
 
-const sim0mqpp::Identifier ready_msg = std::string("READY");
-const sim0mqpp::Identifier time_changed_msg = std::string("TIME_CHANGED_EVENT");
-const sim0mqpp::Identifier gtu_add_msg = std::string("NETWORK.GTU.ADD");
-const sim0mqpp::Identifier gtu_remove_msg = std::string("NETWORK.GTU.REMOVE");
-const sim0mqpp::Identifier gtu_position_msg = std::string("GTUPOSITION");
+const sim0mqpp::Identifier gtu_move_msg = std::string("GTU move");
+const sim0mqpp::Identifier gtu_move_get_current_msg = std::string("GTU move|GET_CURRENT");
+const sim0mqpp::Identifier gtu_network_msg = std::string("GTUs in network");
+const sim0mqpp::Identifier gtu_network_get_current_msg = std::string("GTUs in network|GET_CURRENT");
+const sim0mqpp::Identifier gtu_network_subscribe_add_msg = std::string("GTUs in network|SUBSCRIBE_TO_ADD");
+const sim0mqpp::Identifier gtu_network_subscribe_remove_msg = std::string("GTUs in network|SUBSCRIBE_TO_REMOVE");
+const sim0mqpp::Identifier sim_start_msg = std::string("NEWSIMULATION");
+const sim0mqpp::Identifier sim_stop_msg = std::string("DIE");
+const sim0mqpp::Identifier sim_until_msg = std::string("SIMULATEUNTIL");
+
+// subscription IDs are arbitrarily chosen, just need to be distinct
+const std::int32_t gtu_add_subscription = 0x20201;
+const std::int32_t gtu_remove_subscription = 0x20202;
 
 using namespace omnetpp;
 const simsignal_t gtu_add_signal = cComponent::registerSignal("ots-gtu-add");
@@ -48,6 +57,9 @@ Core::Core()
 
 Core::~Core()
 {
+    if (m_network_loaded) {
+        stopSimulation();
+    }
     cancelAndDelete(m_step_event);
 
     if (m_zmq_socket) {
@@ -79,18 +91,18 @@ void Core::handleMessage(omnetpp::cMessage* msg)
 {
     if (msg == m_step_event) {
         if (!m_network_loaded) {
-            loadNetwork(par("otsNetworkFile").stringValue());
+            startSimulation(par("otsNetworkFile").stringValue());
+            sendCommand(gtu_network_subscribe_add_msg, gtu_add_subscription);
+            sendCommand(gtu_network_subscribe_remove_msg, gtu_remove_subscription);
             m_network_loaded = true;
         }
 
         // trigger OTS to advance by step length
         simulateUntil(omnetpp::simTime());
         scheduleAt(omnetpp::simTime() + m_step_length, m_step_event);
-        queryResponses(); // GTU add and remove events
 
-        // request all GTU positions
+        // request all initial GTU positions
         requestGtuPositions();
-        queryResponses(); // GTU positions
     }
 }
 
@@ -113,14 +125,29 @@ bool Core::receive(std::size_t maxlen, bool block)
     return true;
 }
 
-void Core::sendCommand(const std::string& cmd, std::vector<sim0mqpp::Any>&& payload)
+void Core::sendCommand(const sim0mqpp::Identifier& cmd)
+{
+    sendCommand(cmd, 0);
+}
+
+void Core::sendCommand(const sim0mqpp::Identifier& cmd, std::int32_t cmd_id)
+{
+    sendCommand(cmd, cmd_id, std::vector<sim0mqpp::Any> {});
+}
+
+void Core::sendCommand(const sim0mqpp::Identifier& cmd, std::vector<sim0mqpp::Any>&& payload)
+{
+    sendCommand(cmd, 0, std::move(payload));
+}
+
+void Core::sendCommand(const sim0mqpp::Identifier& cmd, std::int32_t cmd_id, std::vector<sim0mqpp::Any>&& payload)
 {
     sim0mqpp::Message msg;
     msg.federation_id = m_sim_federation;
     msg.sender_id = m_sim_sender;
     msg.receiver_id = m_sim_receiver;
     msg.message_type_id = cmd;
-    msg.message_id = 0;
+    msg.message_id = cmd_id;
     msg.payload = std::move(payload);
 
     sim0mqpp::Buffer buffer;
@@ -135,26 +162,36 @@ void Core::sendCommand(const std::string& cmd, std::vector<sim0mqpp::Any>&& payl
     }
 }
 
-void Core::queryResponses()
+void Core::queryResponses(const sim0mqpp::Identifier& wait_for)
 {
-    bool again = false;
-    while (receive(8192, !again)) {
+    m_pending.insert(wait_for);
+    while (receive(8192, !m_pending.empty())) {
         sim0mqpp::BufferDeserializer input(m_buffer);
         sim0mqpp::Message msg;
         deserialize(input, msg);
         if (input.good()) {
-            if (msg.message_type_id == ready_msg) {
-                EV_TRACE << "READY received from OTS\n";
-                again = true;
-            } else if (msg.message_type_id == time_changed_msg) {
+            m_pending.erase(msg.message_type_id);
+
+            if (msg.message_type_id == sim_until_msg) {
                 // TODO add check if simulation times are in sync as soon as OTS reports time as scalar quantity
-                EV_DETAIL << "time has changed in OTS\n";
-            } else if (msg.message_type_id == gtu_add_msg) {
-                processGtuAdd(msg);
-            } else if (msg.message_type_id == gtu_remove_msg) {
-                processGtuRemove(msg);
-            } else if (msg.message_type_id == gtu_position_msg) {
-                processGtuPosition(msg);
+                EV_DETAIL << "OTS time has changed\n";
+            } else if (msg.message_type_id == gtu_move_msg) {
+                processGtuMove(msg);
+            } else if (msg.message_type_id == gtu_network_msg) {
+                std::int32_t* msg_id = boost::get<std::int32_t>(&msg.message_id);
+                if (msg_id) {
+                    if (*msg_id == 0) {
+                        processCurrentNetwork(msg);
+                    } else if (*msg_id == gtu_add_subscription) {
+                        processGtuAdd(msg);
+                    } else if (*msg_id == gtu_remove_subscription) {
+                        processGtuRemove(msg);
+                    } else {
+                        EV_WARN << "do not know how to process network message with ID " << *msg_id << "\n";
+                    }
+                } else {
+                    EV_ERROR << "network message contains invalid ID\n";
+                }
             } else {
                 EV_WARN << "ignoring message " << sim0mqpp::to_string(msg.message_type_id) << "\n";
             }
@@ -166,9 +203,10 @@ void Core::queryResponses()
 
 void Core::processGtuAdd(const sim0mqpp::Message& msg)
 {
-    const auto id = msg.get_payload<std::string>(0);
+    const auto t = msg.get_payload<sim0mqpp::Unit::Time>(0);
+    const auto id = msg.get_payload<std::string>(1);
     if (id) {
-        EV_TRACE << "add GTU " << *id << "\n";
+        EV_DETAIL << "add GTU " << *id << "\n";
         emit(gtu_add_signal, id->c_str());
     } else {
         EV_ERROR << "cannot read ID of GTU to be added\n";
@@ -177,42 +215,48 @@ void Core::processGtuAdd(const sim0mqpp::Message& msg)
 
 void Core::processGtuRemove(const sim0mqpp::Message& msg)
 {
-    auto id = msg.get_payload<std::string>(0);
+    const auto t = msg.get_payload<sim0mqpp::Unit::Time>(0);
+    const auto id = msg.get_payload<std::string>(1);
     if (id) {
-        EV_TRACE << "remove GTU " << *id << "\n";
+        EV_DETAIL << "remove GTU " << *id << "\n";
         emit(gtu_remove_signal, id->c_str());
     } else {
         EV_ERROR << "cannot read ID of GTU to be removed\n";
     }
 }
 
-void Core::processGtuPosition(const sim0mqpp::Message& msg)
+void Core::processCurrentNetwork(const sim0mqpp::Message& msg)
+{
+    for (const auto& gtu_id : msg.payload) {
+        requestGtuPosition(gtu_id);
+    }
+}
+
+void Core::processGtuMove(const sim0mqpp::Message& msg)
 {
     auto id = msg.get_payload<std::string>(0);
     auto type = msg.get_payload<std::string>(1);
-    auto x = msg.get_payload<double>(2);
-    auto y = msg.get_payload<double>(3);
-    auto z = msg.get_payload<double>(4);
-    auto heading = msg.get_payload<double>(5);
-    auto speed = msg.get_payload<sim0mqpp::Unit::Speed>(6);
-    auto accel = msg.get_payload<sim0mqpp::Unit::Acceleration>(7);
+    auto pos = msg.get_payload<sim0mqpp::Unit::Position, sim0mqpp::VectorQuantity<double>>(2);
+    auto heading = msg.get_payload<sim0mqpp::Unit::Direction>(3);
+    auto speed = msg.get_payload<sim0mqpp::Unit::Speed>(4);
+    auto accel = msg.get_payload<sim0mqpp::Unit::Acceleration>(5);
 
-    if (id && type && x && y && z && heading && speed && accel) {
+    if (id && type && pos && pos->values().size() == 3 && heading && speed && accel) {
         GtuObject gtu;
         gtu.setId(*id);
         gtu.setType(*type);
-        gtu.setPosition({ *x, *y, *z});
-        gtu.setHeadingRad(*heading);
+        gtu.setPosition({ pos->values()[0], pos->values()[1], pos->values()[2] });
+        gtu.setHeadingRad(heading->value());
         gtu.setSpeed(speed->value());
         gtu.setAcceleration(accel->value());
         emit(gtu_position_signal, &gtu);
-        EV_TRACE << "GTU position update for ID " << *id << "\n";
+        EV_DETAIL << "GTU position update for ID " << *id << "\n";
     } else {
         EV_ERROR << "received broken GTU position\n";
     }
 }
 
-void Core::loadNetwork(const std::string& path)
+void Core::startSimulation(const std::string& path)
 {
     std::string ots_network;
     std::ifstream ifs(path);
@@ -231,24 +275,38 @@ void Core::loadNetwork(const std::string& path)
 
     std::vector<sim0mqpp::Any> payload;
     payload.push_back(ots_network);
-    payload.push_back(sim0mqpp::ScalarQuantity<double> { ots_warmup.dbl(), sim0mqpp::Unit::Duration });
     payload.push_back(sim0mqpp::ScalarQuantity<double> { ots_run.dbl(), sim0mqpp::Unit::Duration });
+    payload.push_back(sim0mqpp::ScalarQuantity<double> { ots_warmup.dbl(), sim0mqpp::Unit::Duration });
     payload.push_back(ots_seed);
 
-    sendCommand("LOADNETWORK", std::move(payload));
+    sendCommand(sim_start_msg, std::move(payload));
+}
+
+void Core::stopSimulation()
+{
+    sendCommand(sim_stop_msg);
 }
 
 void Core::simulateUntil(omnetpp::SimTime time)
 {
     std::vector<sim0mqpp::Any> payload;
     payload.push_back(sim0mqpp::ScalarQuantity<double> { time.dbl(), sim0mqpp::Unit::Time });
-    sendCommand("SIMULATEUNTIL", std::move(payload));
+    sendCommand(sim_until_msg, std::move(payload));
+    queryResponses(sim_until_msg);
 }
 
 void Core::requestGtuPositions()
 {
+    sendCommand(gtu_network_get_current_msg);
+    queryResponses(gtu_network_msg);
+}
+
+void Core::requestGtuPosition(const sim0mqpp::Any& gtu_id)
+{
     std::vector<sim0mqpp::Any> payload;
-    sendCommand("SENDALLGTUPOSITIONS", std::move(payload));
+    payload.push_back(gtu_id);
+    sendCommand(gtu_move_get_current_msg, std::move(payload));
+    queryResponses(gtu_move_msg);
 }
 
 } // namespace ots
