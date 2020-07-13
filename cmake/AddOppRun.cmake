c0028896a936588e10497a1d01210c32f7d48f40
Raphael Riebl <raphael@riebl.net>
2020-07-13 23:22:24 +0200

    cmake: generate and install run scripts
    Change-Id: I31307589663d7448f2972bb147bb2889525039c5

--- cmake/AddOppRun.cmake
+++ cmake/AddOppRun.cmake
@@ -8,24 +8,33 @@ set(VALGRIND_EXEC_FLAGS "-u Cmdenv" CACHE STRING "Flags passed to executable run
 set(RUN_FLAGS "" CACHE STRING "Flags appended to run command (and debug)")
 mark_as_advanced(GDB_COMMAND VALGRIND_COMMAND)
 
-function(_get_opp_run_libraries target output)
-    set(libraries "")
+function(_get_opp_run_dependencies target output)
+    set(dependencies "")
     get_target_property(target_type ${target} TYPE)
-    if (${target_type} STREQUAL "SHARED_LIBRARY")
-        get_target_property(opp_library ${target} OMNETPP_LIBRARY)
-        if(opp_library)
-            set(libraries -l $<TARGET_FILE:${target}> ${libraries})
+    if(${target_type} STREQUAL "SHARED_LIBRARY")
+        get_target_property(is_opp_library ${target} OMNETPP_LIBRARY)
+        if(is_opp_library)
+            list(APPEND dependencies ${target})
         endif()
     endif()
 
     get_target_property(interface_libraries ${target} INTERFACE_LINK_LIBRARIES)
     foreach(interface_library IN LISTS interface_libraries)
         if(TARGET ${interface_library})
-            _get_opp_run_libraries(${interface_library} libraries_dependency)
-            set(libraries ${libraries_dependency} ${libraries})
+            _get_opp_run_dependencies(${interface_library} interface_dependencies)
+            list(APPEND dependencies ${interface_dependencies})
         endif()
     endforeach()
 
+    set(${output} ${dependencies} PARENT_SCOPE)
+endfunction()
+
+function(_get_opp_run_libraries target output)
+    set(libraries "")
+    _get_opp_run_dependencies(${target} dependencies)
+    foreach(dependency IN LISTS dependencies)
+        set(libraries -l $<TARGET_FILE:${dependency}> ${libraries})
+    endforeach()
     set(${output} ${libraries} PARENT_SCOPE)
 endfunction()
 
@@ -161,3 +170,52 @@ function(add_opp_test name)
         COMMAND ${exec} ${config} ${opp_run_args}
         WORKING_DIRECTORY ${working_directory})
 endfunction(add_opp_test)
+
+function(generate_run_script)
+    set(option_args "INSTALL")
+    set(one_value_args "TARGET;FILE")
+    cmake_parse_arguments(args "${option_args}" "${one_value_args}" "" ${ARGN})
+
+    if(args_UNPARSED_ARGUMENTS)
+        message(SEND_ERROR "generate_run_script called with invalid arguments: ${args_UNPARSED_ARGUMENTS}")
+    endif()
+
+    if(NOT args_TARGET)
+        message(SEND_ERROR "generate_run_script: TARGET argument is missing")
+    endif()
+
+    if(NOT args_FILE)
+        message(SEND_ERROR "generate_run_script: FILE argument is missing")
+    endif()
+
+    # collect all NED folders for given target
+    if(args_INSTALL)
+        get_install_ned_folders(${args_TARGET} opp_run_ned_folders)
+    else()
+        get_ned_folders(${args_TARGET} opp_run_ned_folders)
+    endif()
+    set(opp_run_ned_folders "$<JOIN:${opp_run_ned_folders},:>")
+
+    # collect libraries for opp_run
+    set(opp_run_libraries "")
+    _get_opp_run_dependencies(${args_TARGET} opp_run_dependencies)
+    foreach(opp_run_dependency IN LISTS opp_run_dependencies)
+        if(args_INSTALL)
+            list(APPEND opp_run_libraries "-l${CMAKE_INSTALL_PREFIX}/lib/$<TARGET_FILE_NAME:${opp_run_dependency}>")
+        else()
+            list(APPEND opp_run_libraries "-l$<TARGET_FILE:${opp_run_dependency}>")
+        endif()
+    endforeach()
+    set(opp_run_libraries "$<JOIN:${opp_run_libraries}, >")
+
+    # select opp_run executable depending on build type
+    if(NOT CMAKE_BUILD_TYPE OR "${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
+        set(opp_run_executable ${OMNETPP_RUN_DEBUG})
+    else()
+        set(opp_run_executable ${OMNETPP_RUN})
+    endif()
+
+    # substitute variables first, then generator expressions
+    configure_file(${PROJECT_SOURCE_DIR}/cmake/run_artery.sh.in ${args_FILE} @ONLY)
+    file(GENERATE OUTPUT ${args_FILE} INPUT ${args_FILE})
+endfunction()
