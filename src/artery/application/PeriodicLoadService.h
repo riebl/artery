d45a3cd7d5b78b048d296361630e18877b155e9a
Raphael Riebl <raphael@riebl.net>
2020-08-31 13:04:04 +0200

    application: add PeriodicLoadService
    Change-Id: Iabd9064d09c9abb8cc03eb92849547c31ab6a774

---
+++ src/artery/application/PeriodicLoadService.h
@@ -0,0 +1,37 @@
+/*
+* Artery V2X Simulation Framework
+* Copyright 2020 Raphael Riebl et al.
+* Licensed under GPLv2, see COPYING file for detailed license and warranty terms.
+*/
+
+#ifndef ARTERY_PERIODICLOADSERVICE_H_
+#define ARTERY_PERIODICLOADSERVICE_H_
+
+#include "artery/application/ItsG5Service.h"
+#include "artery/application/NetworkInterface.h"
+
+namespace artery
+{
+
+class PeriodicLoadService : public ItsG5Service
+{
+    public:
+        PeriodicLoadService();
+        ~PeriodicLoadService();
+
+        void indicate(const vanetza::btp::DataIndication&, omnetpp::cPacket*, const NetworkInterface&) override;
+        void trigger() override;
+
+    protected:
+        void initialize() override;
+        void handleMessage(omnetpp::cMessage*) override;
+        void scheduleTransmission();
+        void generateTransmissionRequest();
+
+    private:
+        omnetpp::cMessage* mTrigger;
+};
+
+} // namespace artery
+
+#endif /* ARTERY_PERIODICLOADSERVICE_H_ */
