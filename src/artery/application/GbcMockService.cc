80323b6bb464415eb6abfd38b9dc54973af65a5f
Raphael Riebl <raphael@riebl.net>
2020-09-18 13:51:07 +0200

    application: add GBC mock service
    Change-Id: Ia416126a191df9738aa793d496d2ee140eeac511

---
+++ src/artery/application/GbcMockService.cc
@@ -0,0 +1,108 @@
+/*
+* Artery V2X Simulation Framework
+* Copyright 2020 Raphael Riebl et al.
+* Licensed under GPLv2, see COPYING file for detailed license and warranty terms.
+*/
+
+#include "artery/application/GbcMockMessage.h"
+#include "artery/application/GbcMockService.h"
+#include <inet/common/geometry/common/Coord.h>
+#include <omnetpp/cmessage.h>
+#include <omnetpp/cpacket.h>
+
+namespace artery
+{
+
+Define_Module(GbcMockService)
+
+using namespace omnetpp;
+
+namespace
+{
+static const simsignal_t scSignalReceived = cComponent::registerSignal("GbcMockReceived");
+static const simsignal_t scSignalSent = cComponent::registerSignal("GbcMockSent");
+} // namespace
+
+GbcMockService::~GbcMockService()
+{
+    cancelAndDelete(mTrigger);
+}
+
+void GbcMockService::initialize()
+{
+    ItsG5Service::initialize();
+    mTrigger = new cMessage("triggger GBC mock message");
+    mPositionProvider = &getFacilities().get_const<PositionProvider>();
+    mHostId = getFacilities().get_const<Identity>().host->getId();
+
+    namespace chrono = std::chrono;
+    namespace si = vanetza::units::si;
+    mGnScf = par("gnStoreCarryForwarding");
+    mGnRadius = par("gnRadius").doubleValue() * si::meter;
+    mGnLifetime.encode(par("gnLifetime").doubleValue() * si::second);
+    mGnRepetitionInterval = par("gnRepetitionInterval").doubleValue() * si::second;
+    mGnRepetitionMaximum = par("gnRepetitionMaximum").doubleValue() * si::second;
+    mDccProfile = par("dccProfile");
+
+    mPacketLimit = par("packetLimit");
+    mMessageLength = par("payloadLength");
+    if (mPacketLimit != 0) {
+        scheduleAt(simTime() + par("generationOffset"), mTrigger);
+    }
+    WATCH(mPacketCounter);
+}
+
+void GbcMockService::handleMessage(cMessage* msg)
+{
+    if (msg == mTrigger) {
+        ++mPacketCounter;
+        generatePacket();
+        if (mPacketLimit < 0 || mPacketLimit > mPacketCounter) {
+            scheduleAt(simTime() + par("generationInterval"), mTrigger);
+        }
+    } else {
+        ItsG5Service::handleMessage(msg);
+    }
+}
+
+void GbcMockService::generatePacket()
+{
+    using namespace vanetza;
+    btp::DataRequestB req;
+    req.destination_port = host_cast<GbcMockService::port_type>(getPortNumber());
+    req.gn.transport_type = geonet::TransportType::GBC;
+    req.gn.traffic_class.tc_id(mDccProfile);
+    req.gn.traffic_class.store_carry_forward(mGnScf);
+    req.gn.communication_profile = geonet::CommunicationProfile::ITS_G5;
+    req.gn.maximum_lifetime = mGnLifetime;
+    if (mGnRepetitionInterval > 0.0 * units::si::second && mGnRepetitionMaximum > 0.0 * units::si::second) {
+        req.gn.repetition = geonet::DataRequest::Repetition {};
+        req.gn.repetition->interval = mGnRepetitionInterval;
+        req.gn.repetition->maximum = mGnRepetitionMaximum;
+    }
+
+    geonet::Area destination;
+    geonet::Circle circle;
+    circle.r = mGnRadius;
+    destination.shape = circle;
+    destination.position.latitude = mPositionProvider->getGeodeticPosition().latitude;
+    destination.position.longitude = mPositionProvider->getGeodeticPosition().longitude;
+    req.gn.destination = destination;
+
+    auto packet = new GbcMockMessage();
+    packet->setSourceStation(mHostId);
+    packet->setSourcePosition(mPositionProvider->getCartesianPosition());
+    packet->setByteLength(mMessageLength);
+    emit(scSignalSent, packet);
+    request(req, packet);
+}
+
+void GbcMockService::indicate(const vanetza::btp::DataIndication&, cPacket* packet)
+{
+    const auto cartesian = mPositionProvider->getCartesianPosition();
+    inet::Coord position { cartesian.x.value(), cartesian.y.value() };
+    emit(scSignalReceived, omnetpp::check_and_cast<GbcMockMessage*>(packet), &position);
+    delete packet;
+}
+
+} // namespace artery
---
+++ src/artery/application/GbcMockService.h
@@ -0,0 +1,45 @@
+/*
+* Artery V2X Simulation Framework
+* Copyright 2020 Raphael Riebl et al.
+* Licensed under GPLv2, see COPYING file for detailed license and warranty terms.
+*/
+
+#ifndef ARTERY_GBCMOCKSERVICE_H_EOU21TYG
+#define ARTERY_GBCMOCKSERVICE_H_EOU21TYG
+
+#include "artery/application/ItsG5Service.h"
+#include "artery/networking/PositionProvider.h"
+#include <vanetza/units/length.hpp>
+
+namespace artery
+{
+
+class GbcMockService : public artery::ItsG5Service
+{
+    public:
+        virtual ~GbcMockService();
+
+    protected:
+        void initialize() override;
+        void handleMessage(omnetpp::cMessage*) override;
+        void indicate(const vanetza::btp::DataIndication&, omnetpp::cPacket*) override;
+        void generatePacket();
+
+    private:
+        int mHostId = 0;
+        const PositionProvider* mPositionProvider = nullptr;
+        omnetpp::cMessage* mTrigger = nullptr;
+        bool mGnScf;
+        vanetza::units::Length mGnRadius;
+        vanetza::geonet::Lifetime mGnLifetime;
+        vanetza::units::Duration mGnRepetitionInterval;
+        vanetza::units::Duration mGnRepetitionMaximum;
+        unsigned mDccProfile = 0;
+        unsigned mMessageLength = 0;
+        int mPacketCounter = 0;
+        int mPacketLimit = -1;
+};
+
+} // namespace artery
+
+#endif /* ARTERY_GBCMOCKSERVICE_H_EOU21TYG */
