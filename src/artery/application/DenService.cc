7a64494e283205e42c9899f92f75e46e05637cb7
Raphael Riebl <raphael@riebl.net>
2020-11-12 12:04:31 +0100

    DenService: add reception and transmission @statistics
    Change-Id: I3564907d8b11bc5b5d3313360282dfef51926a93

--- src/artery/application/DenService.cc
+++ src/artery/application/DenService.cc
@@ -25,6 +25,7 @@ namespace artery
 Define_Module(DenService)
 
 static const simsignal_t denmReceivedSignal = cComponent::registerSignal("DenmReceived");
+static const simsignal_t denmSentSignal = cComponent::registerSignal("DenmSent");
 static const simsignal_t storyboardSignal = cComponent::registerSignal("StoryboardSignal");
 
 DenService::DenService() :
@@ -126,10 +127,14 @@ std::shared_ptr<const den::Memory> DenService::getMemory() const
 void DenService::sendDenm(vanetza::asn1::Denm&& message, vanetza::btp::DataRequestB& request)
 {
     fillRequest(request);
+    DenmObject obj { std::move(message) };
+    emit(denmSentSignal, &obj);
 
     using namespace vanetza;
+    using DenmConvertible = vanetza::convertible::byte_buffer_impl<vanetza::asn1::Denm>;
     std::unique_ptr<geonet::DownPacket> payload { new geonet::DownPacket };
-    payload->layer(OsiLayer::Application) = std::move(message);
+    std::unique_ptr<vanetza::convertible::byte_buffer> denm { new DenmConvertible { obj.shared_ptr() } };
+    payload->layer(OsiLayer::Application) = vanetza::ByteBufferConvertible { std::move(denm) };
     this->request(request, std::move(payload));
 }
 
