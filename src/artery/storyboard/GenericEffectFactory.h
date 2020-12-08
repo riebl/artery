7bf5978faa9e59454de5f4c45a205ae3626aa6d5
Raphael Riebl <raphael@riebl.net>
2020-12-08 09:34:12 +0100

    storyboard: add generic effect factory
    Change-Id: I16f09b7139be828532dfebb89cb0c0fd3b7ff887

---
+++ src/artery/storyboard/GenericEffectFactory.h
@@ -0,0 +1,26 @@
+#ifndef ARTERY_GNERICEFFECTFACTORY_H_ZBQPVHUE
+#define ARTERY_GNERICEFFECTFACTORY_H_ZBQPVHUE
+
+#include "artery/storyboard/EffectFactory.h"
+#include <functional>
+
+namespace artery
+{
+
+class GenericEffectFactory : public EffectFactory
+{
+public:
+    using FactoryFunction = std::function<std::shared_ptr<Effect>(Vehicle&, Story&, ConditionResult&)>;
+
+    GenericEffectFactory(FactoryFunction fn);
+
+    std::shared_ptr<Effect> create(Vehicle& v, Story& s, ConditionResult& r) override;
+
+private:
+    FactoryFunction mFactoryFunction;
+};
+
+} // namespace artery
+
+#endif /* ARTERY_GNERICEFFECTFACTORY_H_ZBQPVHUE */
+
