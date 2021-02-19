riebl/artery

commit 08a5a4d9e0634350dbeecd997b1c4bdb82e1059e

traci: write SUMO log files to result directory by default

src/traci/PosixLauncher.cc
@@ -130,15 +130,19 @@ std::string PosixLauncher::command()
     std::regex port("%PORT%");
     std::regex seed("%SEED%");
     std::regex run("%RUN%");
+    std::regex resultdir("%RESULTDIR%");
 
-    const auto run_number = getSimulation()->getEnvir()->getConfigEx()->getVariable(CFGVAR_RUNNUMBER);
+    const auto cfg = getSimulation()->getEnvir()->getConfigEx();
+    const auto cfg_run_number = cfg->getVariable(CFGVAR_RUNNUMBER);
+    const auto cfg_result_dir = cfg->getVariable(CFGVAR_RESULTDIR);
 
     std::string command = m_command;
     command = std::regex_replace(command, executable, m_executable);
     command = std::regex_replace(command, sumocfg, m_sumocfg);
     command = std::regex_replace(command, port, std::to_string(m_port));
     command = std::regex_replace(command, seed, std::to_string(m_seed));
-    command = std::regex_replace(command, run, run_number);
+    command = std::regex_replace(command, run, cfg_run_number);
+    command = std::regex_replace(command, resultdir, cfg_result_dir);
 
     if (!m_extra_options.empty()) {
       command.append(1, ' ').append(m_extra_options);

src/traci/PosixLauncher.ned
@@ -5,7 +5,7 @@ simple PosixLauncher like Launcher
     parameters:
         @class(traci::PosixLauncher);
         string sumo = default("sumo");
-        string command = default("%SUMO% --remote-port %PORT% --seed %SEED% --configuration-file %SUMOCFG% --message-log sumo-%RUN%.log --no-step-log --quit-on-end");
+        string command = default("%SUMO% --remote-port %PORT% --seed %SEED% --configuration-file %SUMOCFG% --message-log %RESULTDIR%/sumo-%RUN%.log --no-step-log --quit-on-end");
         string sumocfg;
         int port = default(0);
         int seed = default(23423);

commit c4d0995d472af243b827f9b5a2fe5a9109e9f252

traci: ensure that result directory exists before launching SUMO
SUMO fails if the OMNeT++ result directory does not yet exist and this
directory is also the destination of its log file.
This is a regression bug introduced by commit 08a5a4d.

src/traci/PosixLauncher.cc
@@ -81,6 +81,9 @@ ServerEndpoint PosixLauncher::launch()
         m_port = lookupPort();
     }
 
+    // workaround: creates <resultdir> before executing SUMO (for logfile output)
+    recordScalar("port", m_port);
+
     ServerEndpoint endpoint;
     endpoint.hostname = "localhost";
     endpoint.port = m_port;