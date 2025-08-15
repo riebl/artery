e67fd2c4368512c3364b16e2e85802f21e1ae3f4
AshFungor <evgenynarkhov2@gmail.com>
2025-08-15 15:30:34 +0000

    cmake: add requires to add_subdirectory wrapper

--- cmake/AddArterySubdirectory.cmake
+++ cmake/AddArterySubdirectory.cmake
@@ -1,6 +1,15 @@
 function(add_artery_subdirectory directory)
+    # include target subdirectory with certain artery
+    # extension (feature), if the following conditions match:
+    # 1) variable with id matching SWITCH is set to true (feature turned on)
+    # 2) packages that are listed in DEPENDENCIES are found
+    # 3) targets that are listed in REQUIRES are declared (used for extern modules)
+
+    # If condition 1) is not met, then subdirectory is not added and
+    # debug message is issued. If either 2) or 3) fails, then this function raises error.
+
     set(one_value_args SWITCH)
-    set(multi_value_args DEPENDENCIES)
+    set(multi_value_args DEPENDENCIES;REQUIRES)
     cmake_parse_arguments(args "" "${one_value_args}" "${multi_value_args}" ${ARGN})
 
     if(args_UNPARSED_ARGUMENTS)
@@ -17,7 +26,16 @@ function(add_artery_subdirectory directory)
             if(NOT DEFINED ${_dep_var} OR NOT ${_dep_var})
                 message(
                     FATAL_ERROR
-                    "add_artery_subdirectory: required dependency '${dependency}' for '${directory}' not found: variable '${_dep_var}' is not defined"
+                    "add_artery_subdirectory: dependency '${dependency}' for '${directory}' was not found: variable '${_dep_var}' is not defined"
+                )
+            endif()
+        endforeach()
+
+        foreach(required_target IN LISTS args_REQUIRES)
+            if(NOT TARGET ${required_target})
+                message(
+                    FATAL_ERROR
+                    "add_artery_subdirectory: required target '${required_target}' for '${directory}' was not found"
                 )
             endif()
         endforeach()
